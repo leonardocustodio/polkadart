52b3cdec23b6ee170b0dd6d04d372dbd790ba0f2
Kawaljeet Singh <kawal741536@gmail.com>
2022-09-02 21:17:36 +0530

    dummy test

--- packages/polkadart_scale_codec/test/polkadart_scale_codec_test.dart
+++ packages/polkadart_scale_codec/test/polkadart_scale_codec_test.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('Dummy test', () {
+    test('true == true', () {
+      expect(true, equals(true));
+    });
+  });
+}
