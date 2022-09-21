7ff5302271144b703107237c440cee40a16cf04f
Kawaljeet Singh <kawal741536@gmail.com>
2022-09-21 19:54:34 +0530

    simplified test cases

--- packages/polkadart_scale_codec/test/chain_test.dart
+++ packages/polkadart_scale_codec/test/chain_test.dart
@@ -2,165 +2,38 @@ import 'package:test/test.dart';
 import './test_chain/chain.dart';
 
 void main() {
-  group('Chain: acala', () {
-    var obj = Chain('acala');
+  final chains = <String>[
+    'acala',
+    'altair',
+    'astar',
+    'basilisk',
+    'bifrost',
+    'calamari',
+    'clover',
+    'crust',
+    'darwinia',
+    'heiko',
+    'hydradx',
+    'karura',
+    'khala',
+    'kilt',
+    'kintsugi',
+    'kusama',
+    'moonriver',
+    'parallel',
+    'pioneer',
+    'polkadot',
+    'quartz',
+    'shibuya',
+    'shiden',
+    'statemine',
+    'statemint',
+    'subsocial',
+  ];
+
+  for (var chain in chains) {
+    var obj = Chain(chain);
     obj.testConstantsScaleEncodingDecoding();
     obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: altair', () {
-    var obj = Chain('altair');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: astar', () {
-    var obj = Chain('astar');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: basilisk', () {
-    var obj = Chain('basilisk');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: bifrost', () {
-    var obj = Chain('bifrost');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: calamari', () {
-    var obj = Chain('calamari');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: clover', () {
-    var obj = Chain('clover');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: crust', () {
-    var obj = Chain('crust');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: darwinia', () {
-    var obj = Chain('darwinia');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: heiko', () {
-    var obj = Chain('heiko');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: hydradx', () {
-    var obj = Chain('hydradx');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: karura', () {
-    var obj = Chain('karura');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: khala', () {
-    var obj = Chain('khala');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: kilt', () {
-    var obj = Chain('kilt');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: kintsugi', () {
-    var obj = Chain('kintsugi');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: kusama', () {
-    var obj = Chain('kusama');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: moonriver', () {
-    var obj = Chain('moonriver');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: parallel', () {
-    var obj = Chain('parallel');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: pioneer', () {
-    var obj = Chain('pioneer');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: polkadot', () {
-    var obj = Chain('polkadot');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: quartz', () {
-    var obj = Chain('quartz');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: shibuya', () {
-    var obj = Chain('shibuya');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: shiden', () {
-    var obj = Chain('shiden');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: statemine', () {
-    var obj = Chain('statemine');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  group('Chain: statemint', () {
-    var obj = Chain('statemint');
-    obj.testConstantsScaleEncodingDecoding();
-    obj.testEventsScaleEncodingDecoding();
-  });
-
-  /// TODO: Resolve Conflicts of this package at
-  ///
-  /// description.specversion: `1`,
-  /// pallet: `Scores`
-  /// name: `CommentVoteDownAction`
-  ///
-  /// group('Chain: subsocial', () {
-  ///  var obj = Chain('subsocial');
-  ///  obj.testConstantsScaleEncodingDecoding();
-  ///  obj.testEventsScaleEncodingDecoding();
-  ///});
+  }
 }
