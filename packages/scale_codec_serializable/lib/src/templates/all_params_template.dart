b8cf003f71fd5b451dd24cbb6b2a92f185e6acc2
gabrielokura <gabriellevicz@gmail.com>
2022-09-26 19:41:06 -0300

    resolve all_params_template naming issue

--- packages/scale_codec_serializable/lib/src/templates/all_params_template.dart
+++ packages/scale_codec_serializable/lib/src/templates/all_params_template.dart
@@ -1,15 +1,15 @@
-import 'package:source_gen/source_gen.dart';
-
 import '../models/param.dart';
 import 'param_template.dart';
 
+/// Template used to write the declaration of all [ParamTemplate] of a class.
 class AllParamsTemplate {
   final Iterable<Param> params;
 
   const AllParamsTemplate(this.params);
 
-  String generateParams() => _generateParams((param) => param.generateParam());
-
+  /// Used to write all parameters in constructors declaration.
+  ///
+  /// Check an example of use on `DecodeMethodTemplate`.
   String generateParamsUsage() {
     final positional = params
         .where((element) => element.isPositional)
@@ -24,37 +24,15 @@ class AllParamsTemplate {
     return [...positional, ...named].join(',');
   }
 
+  /// Generate new encoded fields for each [ParamTemplate].
+  ///
+  /// Example:
+  /// ```
+  /// ...
+  /// String exampleEncoded = '';
+  /// ```
   String generateEncodedFields() => params
       .map(ParamTemplate.new)
       .map((e) => e.generateEncodedField())
       .join('\n');
-
-  String _generateParams(
-    String Function(ParamTemplate) paramGeneratorSelector,
-  ) {
-    final paramTemplates = params.map(ParamTemplate.new);
-    final positional =
-        paramTemplates.where((element) => element.param.isRequiredPositional);
-    final optional =
-        paramTemplates.where((element) => element.param.isOptionalPositional);
-    final named = paramTemplates.where((element) => element.param.isNamed);
-
-    if (optional.isNotEmpty && named.isNotEmpty) {
-      throw InvalidGenerationSourceError(
-          '[EROR] Method or constructor has optional positional and named params');
-    }
-
-    final positionalParams = positional.map(paramGeneratorSelector).join(',');
-    final optionalParams = optional.isNotEmpty
-        ? '[${optional.map(paramGeneratorSelector).join(',')}]'
-        : null;
-    final namedParams = named.isNotEmpty
-        ? '{${named.map(paramGeneratorSelector).join(',')}}'
-        : null;
-
-    return [positionalParams, optionalParams, namedParams]
-        .whereType<String>()
-        .where((element) => element.isNotEmpty)
-        .join(',');
-  }
 }
