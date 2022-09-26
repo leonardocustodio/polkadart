3f69cb8e2f65978c531f1680034f901a06e92c34
gabrielokura <gabriellevicz@gmail.com>
2022-09-26 16:55:23 -0300

    change optional booleans in ScaleCodecSerializable

--- packages/scale_codec_annotation/lib/src/scale_codec_serializable.dart
+++ packages/scale_codec_annotation/lib/src/scale_codec_serializable.dart
@@ -1,25 +1,32 @@
 class ScaleCodecSerializable {
   /// If `true` (the default), a static `.decode()` method
   /// is created in the generated part file.
-  final bool? shouldCreateDecodeMethod;
+  final bool shouldCreateDecodeMethod;
 
   /// If `true` (the default), a static `.encode()` method is created in
   /// the generated part file.
-  final bool? shouldCreateEncodeMethod;
+  final bool shouldCreateEncodeMethod;
 
   /// Creates a new [ScaleCodecSerializable] instance
   const ScaleCodecSerializable({
-    this.shouldCreateDecodeMethod,
-    this.shouldCreateEncodeMethod,
+    this.shouldCreateDecodeMethod = true,
+    this.shouldCreateEncodeMethod = true,
   });
 
   /// Creates a new [ScaleCodecSerializable] instance.
   ///
-  /// Used to load data from [BuilderOptions] on `build.yaml`.
+  ///  If any map key wasn't found, the default value will be used instead.
+  ///
+  /// _OBS: Used to load data from [BuilderOptions] on `build.yaml`_.
   factory ScaleCodecSerializable.fromMap(Map<String, dynamic> map) {
+    final shouldCreateDecodeMethod =
+        map['should_create_decode_method'] as bool?;
+    final shouldCreateEncodeMethod =
+        map['should_create_encode_method'] as bool?;
+
     return ScaleCodecSerializable(
-      shouldCreateDecodeMethod: map['should_create_decode_method'] as bool?,
-      shouldCreateEncodeMethod: map['should_create_encode_method'] as bool?,
+      shouldCreateDecodeMethod: shouldCreateDecodeMethod ?? true,
+      shouldCreateEncodeMethod: shouldCreateEncodeMethod ?? true,
     );
   }
 }
--- packages/scale_codec_serializable/lib/src/utils/utils.dart
+++ packages/scale_codec_serializable/lib/src/utils/utils.dart
@@ -3,15 +3,24 @@ import 'package:source_gen/source_gen.dart';
 
 import '../config.dart';
 
+/// Used to define types for [_valueForAnnotation].
+class _Annotations {
+  bool? shouldCreateDecodeMethod;
+  bool? shouldCreateEncodeMethod;
+  _Annotations(this.shouldCreateDecodeMethod, this.shouldCreateEncodeMethod);
+}
+
 /// Return an instance of [ScaleCodecSerializable] corresponding to the
 /// provided [reader].
 // #CHANGE WHEN UPDATING scale_codec_annotation
-ScaleCodecSerializable _valueForAnnotation(ConstantReader reader) =>
-    ScaleCodecSerializable(
-        shouldCreateDecodeMethod:
-            reader.read('shouldCreateDecodeMethod').literalValue as bool?,
-        shouldCreateEncodeMethod:
-            reader.read('shouldCreateEncodeMethod').literalValue as bool?);
+_Annotations _valueForAnnotation(ConstantReader reader) {
+  final shouldCreateDecodeMethod =
+      reader.read('shouldCreateDecodeMethod').literalValue as bool?;
+  final shouldCreateEncodeMethod =
+      reader.read('shouldCreateEncodeMethod').literalValue as bool?;
+
+  return _Annotations(shouldCreateDecodeMethod, shouldCreateEncodeMethod);
+}
 
 /// Returns a [ClassConfig] with values from the [ScaleCodecSerializable]
 /// instance represented by [reader].
