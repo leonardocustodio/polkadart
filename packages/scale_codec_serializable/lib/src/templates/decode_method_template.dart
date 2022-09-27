41b27c762052087b6732dc6feb4ccfdf026a2d22
gabrielokura <gabriellevicz@gmail.com>
2022-09-27 09:25:52 -0300

    add todo on methods

--- packages/scale_codec_serializable/lib/src/templates/decode_method_template.dart
+++ packages/scale_codec_serializable/lib/src/templates/decode_method_template.dart
@@ -2,6 +2,7 @@ import '../models/scale_codec_class.dart';
 import 'all_params_template.dart';
 
 extension DecodeMethodTemplate on ScaleCodecClass {
+  //TODO: implement decode method and test
   String generateDecodeMethod() {
     if (!shouldCreateDecodeMethod) {
       return '';
--- packages/scale_codec_serializable/lib/src/templates/encode_method_template.dart
+++ packages/scale_codec_serializable/lib/src/templates/encode_method_template.dart
@@ -1,6 +1,7 @@
 import '../models/scale_codec_class.dart';
 
 extension EncodeMethodTemplate on ScaleCodecClass {
+  //TODO: implement encode method and test
   String generateEncodeMethod() {
     if (!shouldCreateEncodeMethod) {
       return '';
