e6341c6e635b34a702e7dafb951e5bb2b336b74d
Gabriel Okura <gabriel.okura@sejahype.com.br>
2022-10-05 11:58:56 -0300

    remove prints from example file

--- packages/ss58/example/ss58_example.dart
+++ packages/ss58/example/ss58_example.dart
@@ -1,23 +1,21 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:ss58/ss58.dart';
 
 void main() {
   // get registry info of given `network`
   final kusamaRegistry = Codec.registry.getByNetwork('kusama');
-  print('kusama registry: $kusamaRegistry');
 
   // get registry info of given `prefix`
   final polkadotRegistry = Codec.registry.getByPrefix(0);
-  print('polkadot registry: $polkadotRegistry');
 
   // decoding substrate address
   final String originalEncodedAddress =
       '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
   final List<int> decodedBytes =
       Codec.fromNetwork('substrate').decode(originalEncodedAddress);
-  print('Substrate address bytes: $decodedBytes');
 
   // Encoding the decodedBytes to produce back encodedAddress.
   final int substrateAddressPrefix = 42;
   final encodedAddress = Codec(substrateAddressPrefix).encode(decodedBytes);
-  print(encodedAddress);
 }
