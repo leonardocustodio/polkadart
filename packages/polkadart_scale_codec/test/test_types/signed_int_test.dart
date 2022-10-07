0594087bcb5b24a7546eca7b8406d4820e5a2da7
gabrielokura <gabriellevicz@gmail.com>
2022-10-07 17:07:56 -0300

    encode signed integers tests

--- packages/polkadart_scale_codec/test/test_types/signed_int_test.dart
+++ packages/polkadart_scale_codec/test/test_types/signed_int_test.dart
@@ -1,13 +1,14 @@
-import 'package:polkadart_scale_codec/polkadart_scale_codec.dart';
+import 'package:polkadart_scale_codec/src/core/core.dart';
+import 'package:polkadart_scale_codec/src/util/utils.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('encode to hex unsigned int tests', () {
+  group('encode to hex signed int tests', () {
     final typesRegistry = <String, dynamic>{
       'Codec': {
-        'a': 'u8',
-        'b': 'u16',
-        'c': 'u32',
+        'a': 'i8',
+        'b': 'i16',
+        'c': 'i32',
       },
     };
     // Creates the registry for parsing the types and selecting particular schema.
@@ -27,16 +28,16 @@ void main() {
         int value = 69;
         String expectedResult = '0x45';
 
-        int registryIndex = registry.getIndex('u8');
+        int registryIndex = registry.getIndex('i8');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
 
-      test('Should return correct encoded data when value is zero', () {
-        int value = 0;
-        String expectedResult = '0x00';
+      test('Should return correct encoded data when value is -128', () {
+        int value = -128;
+        String expectedResult = '0x80';
 
-        int registryIndex = registry.getIndex('u8');
+        int registryIndex = registry.getIndex('i8');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
@@ -45,15 +46,15 @@ void main() {
         int value = 127;
         String expectedResult = '0x7f';
 
-        int registryIndex = registry.getIndex('u8');
+        int registryIndex = registry.getIndex('i8');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
 
-      test('Should throw InvalidSizeException when value is smaller than zero',
+      test('Should throw InvalidSizeException when value is smaller than -128',
           () {
-        int value = -1;
-        int registryIndex = registry.getIndex('u8');
+        int value = -129;
+        int registryIndex = registry.getIndex('i8');
 
         expect(
           () => codec.encodeToHex(registryIndex, value),
@@ -64,7 +65,7 @@ void main() {
       test('Should throw InvalidSizeException when value is greater than 127',
           () {
         int value = 128;
-        int registryIndex = registry.getIndex('u8');
+        int registryIndex = registry.getIndex('i8');
 
         expect(
           () => codec.encodeToHex(registryIndex, value),
@@ -78,16 +79,16 @@ void main() {
         int value = 42;
         String expectedResult = '0x2a00';
 
-        int registryIndex = registry.getIndex('u16');
+        int registryIndex = registry.getIndex('i16');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
 
-      test('Should return correct encoded data when value is zero', () {
-        int value = 0;
-        String expectedResult = '0x0000';
+      test('Should return correct encoded data when value is -32768', () {
+        int value = -32768;
+        String expectedResult = '0x0080';
 
-        int registryIndex = registry.getIndex('u16');
+        int registryIndex = registry.getIndex('i16');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
@@ -96,15 +97,15 @@ void main() {
         int value = 32767;
         String expectedResult = '0xff7f';
 
-        int registryIndex = registry.getIndex('u16');
+        int registryIndex = registry.getIndex('i16');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
 
-      test('Should throw InvalidSizeException when value is smaller than zero',
+      test('Should throw InvalidSizeException when value is smaller than -32768',
           () {
-        int value = -1;
-        int registryIndex = registry.getIndex('u16');
+        int value = -32769;
+        int registryIndex = registry.getIndex('i16');
 
         expect(
           () => codec.encodeToHex(registryIndex, value),
@@ -115,7 +116,7 @@ void main() {
       test('Should throw InvalidSizeException when value is greater than 32767',
           () {
         int value = 32768;
-        int registryIndex = registry.getIndex('u16');
+        int registryIndex = registry.getIndex('i16');
 
         expect(
           () => codec.encodeToHex(registryIndex, value),
@@ -129,16 +130,16 @@ void main() {
         int value = 16777215;
         String expectedResult = '0xffffff00';
 
-        int registryIndex = registry.getIndex('u32');
+        int registryIndex = registry.getIndex('i32');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
 
-      test('Should return correct encoded data when value is zero', () {
-        int value = 0;
-        String expectedResult = '0x00000000';
+      test('Should return correct encoded data when value is -2147483648', () {
+        int value = -2147483648;
+        String expectedResult = '0x00000080';
 
-        int registryIndex = registry.getIndex('u32');
+        int registryIndex = registry.getIndex('i32');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
@@ -147,15 +148,15 @@ void main() {
         int value = 2147483647;
         String expectedResult = '0xffffff7f';
 
-        int registryIndex = registry.getIndex('u32');
+        int registryIndex = registry.getIndex('i32');
 
         expect(codec.encodeToHex(registryIndex, value), expectedResult);
       });
 
-      test('Should throw InvalidSizeException when value is smaller than zero',
+      test('Should throw InvalidSizeException when value is smaller than -2147483648',
           () {
-        int value = -1;
-        int registryIndex = registry.getIndex('u32');
+        int value = -2147483649;
+        int registryIndex = registry.getIndex('i32');
 
         expect(
           () => codec.encodeToHex(registryIndex, value),
@@ -166,7 +167,7 @@ void main() {
       test('Should throw InvalidSizeException when value is greater than 2147483647',
           () {
         int value = 2147483648;
-        int registryIndex = registry.getIndex('u32');
+        int registryIndex = registry.getIndex('i32');
 
         expect(
           () => codec.encodeToHex(registryIndex, value),
