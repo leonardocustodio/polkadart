14ca803d02cfce5965fa64609f3970544245e993
Kawaljeet Singh <kawal741536@gmail.com>
2022-10-13 00:06:08 +0530

    add inline ignore statements

--- packages/substrate_metadata/lib/old/definitions/metadata/v12.dart
+++ packages/substrate_metadata/lib/old/definitions/metadata/v12.dart
@@ -2,6 +2,8 @@
 ///
 /// Referenced from: https://github.com/subsquid/squid/blob/%40subsquid/substrate-metadata_v1.1.0/substrate-metadata/src/old/definitions/metadata/v12.ts
 
+// ignore_for_file: constant_identifier_names
+
 part of metadata;
 
 const V12 = <String, dynamic>{
