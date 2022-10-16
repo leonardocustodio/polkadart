1622221720c7af7adf8378f4e6f90f38cd94c185
Gabriel Okura <gabriellevicz@gmail.com>
2022-10-16 18:57:56 -0300

    fix tests

--- packages/polkadart_scale_codec/test/test_types/codec_i32_test.dart
+++ packages/polkadart_scale_codec/test/test_types/codec_i32_test.dart
@@ -2,65 +2,65 @@ import 'package:polkadart_scale_codec/polkadart_scale_codec.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('encode signed 16-bit to hex', () {
+  group('encode signed 32-bit to hex', () {
     test(
         'Given a positive integer when it is within range it should be encoded',
         () {
-      const value = 42;
-      const expectedResult = '0x2a00';
+      const value = 16777215;
+      const expectedResult = '0xffffff00';
 
-      expect(CodecI16().encodeToHex(value), expectedResult);
+      expect(CodecI32().encodeToHex(value), expectedResult);
     });
 
     test(
         'Given a negative integer when it is within range it should be encoded',
         () {
-      const value = -32768;
-      const expectedResult = '0x0080';
+      const value = -2147483648;
+      const expectedResult = '0x00000080';
 
-      expect(CodecI16().encodeToHex(value), expectedResult);
+      expect(CodecI32().encodeToHex(value), expectedResult);
     });
 
     test('Should return correct encoded data when value is zero', () {
       const value = 0;
-      const expectedResult = '0x0000';
+      const expectedResult = '0x00000000';
 
-      expect(CodecI16().encodeToHex(value), expectedResult);
+      expect(CodecI32().encodeToHex(value), expectedResult);
     });
 
     test(
-        'Should return correct encoded data when value fits 16 bits and is positive',
+        'Should return correct encoded data when value fits 32 bits and is positive',
         () {
-      const largestSupportedValue = 32767;
-      const expectedResult = '0xff7f';
+      const largestSupportedValue = 2147483647;
+      const expectedResult = '0xffffff7f';
 
-      expect(CodecI16().encodeToHex(largestSupportedValue), expectedResult);
+      expect(CodecI32().encodeToHex(largestSupportedValue), expectedResult);
     });
 
     test(
-        'Should return correct encoded data when value fits 16 bits and is negative',
+        'Should return correct encoded data when value fits 32 bits and is negative',
         () {
-      const smallestSupportedValue = -32768;
-      const expectedResult = '0x0080';
+      const smallestSupportedValue = -2147483648;
+      const expectedResult = '0x00000080';
 
-      expect(CodecI16().encodeToHex(smallestSupportedValue), expectedResult);
+      expect(CodecI32().encodeToHex(smallestSupportedValue), expectedResult);
     });
 
     test(
-        "Given an 16 bit decoder when value is negative and can't be represented it should throw",
+        "Given an 32 bit decoder when value is negative and can't be represented it should throw",
         () {
-      const value = -32769;
+      const value = -2147483649;
 
       expect(
-        () => CodecI16().encodeToHex(value),
+        () => CodecI32().encodeToHex(value),
         throwsA(isA<InvalidSizeException>()),
       );
     });
 
     test(
-        "Given an 16 bit decoder when value is positive and can't be represented it should throw",
+        "Given an 32 bit decoder when value is positive and can't be represented it should throw",
         () {
-      const value = 32768;
+      const value = 2147483648;
 
       expect(
         () => CodecI16().encodeToHex(value),
@@ -69,7 +69,7 @@ void main() {
     });
   });
 
-  group('decode signed 16-bit from hex', () {
+  group('decode signed 32-bit from hex', () {
     test('Given an encoded string when it represents zero it should be decoded',
         () {
       const value = '0x00000000';
@@ -93,13 +93,13 @@ void main() {
       const smallestSupportedValue = '0x00000080';
       const expectedResult = -2147483648;
 
-      expect(CodecI16().decodeFromHex(smallestSupportedValue), expectedResult);
+      expect(CodecI32().decodeFromHex(smallestSupportedValue), expectedResult);
     });
 
     test(
         "Given an encoded string when it represents a integer that don't fit 32 bits it should throw",
         () {
-      const value = '0x80000000';
+      const value = '0xffffff0000000000';
 
       expect(
         () => CodecI32().decodeFromHex(value),
