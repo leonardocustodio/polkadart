95f5b3e9b905797a7130b33520d455991ec9abac
Gabriel Okura <gabriellevicz@gmail.com>
2022-10-17 14:40:36 -0300

    update README and example folder

--- packages/ss58_codec/README.md
+++ packages/ss58_codec/README.md
@@ -1,3 +1,25 @@
-## ss58_codec
+## SS58 Codec
 
-In Progress...
+Provides encoder and decoder used by [SS58] to parse [substrate](https://docs.substrate.io/fundamentals/accounts-addresses-keys/) addresses.
+
+## Usage
+
+To use this package, add `ss58_codec` as a [dependency in your pubspec.yaml file](https://docs.flutter.dev/development/platform-integration/platform-channels).
+
+### Example
+
+```dart
+import 'package:ss58_codec/ss58_codec.dart';
+
+void main() {
+  // Decoding a Polkadot Address Object
+  Address address =
+      SS58Codec.decode('1zugcag7cJVBtVRnFxv5Qftn7xKAnR6YJ9x4x3XLgGgmNnS');
+  print('Address: $address');
+
+  // Encoding an Address Object
+  String encodedAddress = SS58Codec.encode(address);
+  print('Encoded address: $encodedAddress');
+}
+
+```
--- packages/ss58_codec/example/ss58_codec_example.dart
+++ packages/ss58_codec/example/ss58_codec_example.dart
@@ -1,22 +1,12 @@
-import 'dart:typed_data';
-
-import 'package:convert/convert.dart';
 import 'package:ss58_codec/ss58_codec.dart';
 
 void main() {
-  // Decoding an Polkadot Address Object
-  Address decodedAddress =
+  // Decoding a Polkadot Address Object
+  Address address =
       SS58Codec.decode('1zugcag7cJVBtVRnFxv5Qftn7xKAnR6YJ9x4x3XLgGgmNnS');
-  print('Decoding Polkadot Address');
-  print('$decodedAddress\n');
+  print('Address: $address');
 
   // Encoding an Address Object
-  Address addressObject = Address(
-      prefix: 42,
-      bytes: Uint8List.fromList(hex.decode(
-          'd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d')));
-  String encodedAddress = SS58Codec.encode(addressObject);
-
-  // expected: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-  print('Encode Address Object: $encodedAddress');
+  String encodedAddress = SS58Codec.encode(address);
+  print('Encoded address: $encodedAddress');
 }
