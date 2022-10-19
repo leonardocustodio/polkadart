dc15129af9e9dc8fb77909d2c74423aad892a669
Gabriel Okura <gabriellevicz@gmail.com>
2022-10-19 17:08:23 -0300

    update codecU128 documentation

--- packages/polkadart_scale_codec/lib/src/types/codec_u128.dart
+++ packages/polkadart_scale_codec/lib/src/types/codec_u128.dart
@@ -4,14 +4,15 @@ part of '../core/core.dart';
 ///
 /// Basic integers are encoded using a fixed-width little-endian (LE) format.
 ///
-/// Example:
-/// ```
-/// final encoded = CodecU128.encode(16777215);
-/// final decoded = CodecU128.decode("0xffffff00000000000000000000000000");
-/// ```
-///
 /// See also: https://docs.substrate.io/reference/scale-codec/
 class CodecU128 implements ScaleCodecType<BigInt> {
+  /// Returns an encoded hex-decimal `string` from `BigInt` [value]
+  /// using [HexEncoder] methods.
+  ///
+  /// Example:
+  /// ```
+  /// final encoded = CodecU128.encodeToHex(16777215.toBigInt); // "0xffffff00000000000000000000000000"
+  /// ```
   @override
   String encodeToHex(value) {
     var sink = HexEncoder();
@@ -19,6 +20,13 @@ class CodecU128 implements ScaleCodecType<BigInt> {
     return sink.toHex();
   }
 
+  /// Returns an `BigInt` value which the hex-decimal `string`
+  /// [encodedData] represents, using [Source] methods.
+  ///
+  /// Example:
+  /// ```
+  /// final decoded = CodecU128.decodeFromHex("0xffffff00000000000000000000000000"); // 16777215
+  /// ```
   @override
   BigInt decodeFromHex(String encodedData) {
     Source source = Source(encodedData);
