1a7bcbad75763a2d7db081a6cfa5456111a4733e
Kawaljeet Singh <kawal741536@gmail.com>
2022-10-19 22:44:13 +0530

    convert dart file to json file

--- packages/substrate_metadata/test/metadata/metadata_decoder_test.dart
+++ packages/substrate_metadata/test/metadata/metadata_decoder_test.dart
@@ -1,15 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:substrate_metadata/metadata_decoder.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 import 'metadata_hex.dart';
-import 'metadata_json.dart';
 
 void main() {
   group('Test MetadataDecoder', () {
-    test('decoding of metadata', () {
+    test('When valid hexa-decimal is decoded then it produces valid metadata.',
+        () {
       var decoder = MetadataDecoder.instance;
       var decodedMetadata = decoder.decode(metadata_hex);
-      expect(decodedMetadata, metadata_json);
+
+      final file = File('../substrate_metadata/test/metadata/metadata.json');
+      final expectedMetadataJson = jsonDecode(file.readAsStringSync());
+
+      expect(decodedMetadata, expectedMetadataJson);
     });
   });
 }
