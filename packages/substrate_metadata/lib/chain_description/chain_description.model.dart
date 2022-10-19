c063319185ccffbdef132330fc2d295d137eb4be
Kawaljeet Singh <kawal741536@gmail.com>
2022-10-20 00:13:40 +0530

    add get chain description from metadata

--- packages/substrate_metadata/lib/chain_description/chain_description.model.dart
+++ packages/substrate_metadata/lib/chain_description/chain_description.model.dart
@@ -1,8 +1,12 @@
 import 'dart:typed_data';
-
+import 'package:substrate_metadata/storage/storage_item.model.dart';
+import 'package:substrate_metadata/chain_description/parse_legacy.dart';
+import 'package:substrate_metadata/chain_description/parse_v14.dart';
+import 'package:substrate_metadata/models/models.dart';
+import 'package:substrate_metadata/old/legacy_types_model.dart';
+import 'package:substrate_metadata/utils/utils.dart';
 import 'package:polkadart_scale_codec/polkadart_scale_codec.dart'
     as scale_codec;
-import 'package:substrate_metadata/storage/storage_item.model.dart';
 
 class ChainDescription {
   final List<scale_codec.Type> types;
@@ -28,6 +32,24 @@ class ChainDescription {
     required this.storage,
     required this.constants,
   });
+
+  static ChainDescription getFromMetadata(
+      Metadata metadata, LegacyTypes? legacyTypes) {
+    switch (metadata.kind) {
+      case 'V9':
+      case 'V10':
+      case 'V11':
+      case 'V12':
+      case 'V13':
+        assertionCheck(legacyTypes != null,
+            'Type definitions are required for metadata ${metadata.kind}');
+        return ParseLegacy(metadata, legacyTypes!).getChainDescription();
+      case 'V14':
+        return ParseV14((metadata as Metadata_V14).value).getChainDescription();
+      default:
+        throw Exception('Unsupported metadata version: ${metadata.kind}');
+    }
+  }
 }
 
 class Constant {
