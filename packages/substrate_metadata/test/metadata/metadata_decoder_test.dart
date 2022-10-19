fa5f2f754f7108be1c6dc66822a4d8b8f1d9a936
Kawaljeet Singh <kawal741536@gmail.com>
2022-10-19 22:47:11 +0530

    remove json file

--- packages/substrate_metadata/test/metadata/metadata_decoder_test.dart
+++ packages/substrate_metadata/test/metadata/metadata_decoder_test.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
 import 'package:substrate_metadata/metadata_decoder.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
@@ -9,13 +7,9 @@ void main() {
   group('Test MetadataDecoder', () {
     test('When valid hexa-decimal is decoded then it produces valid metadata.',
         () {
-      var decoder = MetadataDecoder.instance;
-      var decodedMetadata = decoder.decode(metadata_hex);
+      final decoder = MetadataDecoder.instance;
 
-      final file = File('../substrate_metadata/test/metadata/metadata.json');
-      final expectedMetadataJson = jsonDecode(file.readAsStringSync());
-
-      expect(decodedMetadata, expectedMetadataJson);
+      expect(() => decoder.decode(metadata_hex), returnsNormally);
     });
   });
 }
