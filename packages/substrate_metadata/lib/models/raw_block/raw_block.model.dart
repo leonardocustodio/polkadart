628d49979bd76d1cd00ba0fc4987c900ccf1dbf2
Kawaljeet Singh <49296873+justkawal@users.noreply.github.com>
2022-11-29 17:30:44 +0530

    Adds extrinsic, decode/encode (#131)
    * change ref to versioned one and add V9 metadata
    
    * add v10
    
    * add v11
    
    * add v12
    
    * add v13
    
    * add base
    
    * allow const names to be cases insensitive
    
    * add v14
    
    * add OldTypes class
    
    * combined call v9 - v14 versions from metadata-versions.dart
    
    * ref metadata files as a small lib.
    
    * add xcm v0
    
    * add xcm v1
    
    * add xcm v2
    
    * add xcm src to access xcm map as entry point
    
    * remove analysis rules
    
    * add inline ignore statements
    
    * renaming of oldtype to legacytype
    
    * add references of schema, rename to LegacyTypes,
    
    * add spec version
    
    * document
    
    * add events, calls, type hashing, exceptions
    
    * rename to event registry
    
    * remove abbreviations
    
    * adds models for substrate's types bundles
    
    * split in two files
    
    * merge common_utils to utils
    
    * Update packages/substrate_metadata/pubspec.yaml
    
    Co-authored-by: Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
    
    * add metadata decoder
    
    * add metadata decoder, add tests, update tests.yml
    
    * convert dart file to json file
    
    * remove json file
    
    * renamed chain to chain_description, added v14 and legacy parser to convert metadata to chain description
    
    * add get chain description from metadata
    
    * add encode/decode extrinsic and byte encoder
    
    * flatten functionality
    
    * add extrinsic functionality
    
    * change arguments
    
    * convert to non-singleton
    
    * changes
    
    * singleton is better to avoid repeated heavy task
    
    * remove singleton
    
    * remove comment code
    
    * rename to decodedBlock
    
    * formatting
    
    * change documentation
    
    * remove generated code
    
    * improve github workflow
    
    * add delete conflicting outputs
    
    * remove generated class, use show as referencing...
    
    * readme documentation
    
    * formatting Readme
    
    Co-authored-by: Lohann Paterno Coutinho Ferreira <developer@lohann.dev>

--- packages/substrate_metadata/lib/models/raw_block/raw_block.model.dart
+++ packages/substrate_metadata/lib/models/raw_block/raw_block.model.dart
@@ -1,24 +1,36 @@
 part of models;
 
-class RpcBlockDigest {
+class RpcBlockDigest implements Equatable {
   final List<String> logs;
   const RpcBlockDigest({required this.logs});
 
-  static RpcBlockDigest fromMap(Map<String, dynamic> map) =>
+  static RpcBlockDigest fromJson(Map<String, dynamic> map) =>
       RpcBlockDigest(logs: (map['logs'] as List).cast<String>());
+
+  @override
+  List<Object?> get props => [logs];
+
+  @override
+  bool? get stringify => true;
 }
 
-class RpcBlockHeader {
+class RpcBlockHeader implements Equatable {
   final String number;
   final String parentHash;
   final RpcBlockDigest digest;
   const RpcBlockHeader(
       {required this.number, required this.parentHash, required this.digest});
 
-  static RpcBlockHeader fromMap(Map<String, dynamic> map) => RpcBlockHeader(
+  static RpcBlockHeader fromJson(Map<String, dynamic> map) => RpcBlockHeader(
       number: map['number'],
       parentHash: map['parentHash'],
-      digest: RpcBlockDigest.fromMap(map['digest']));
+      digest: RpcBlockDigest.fromJson(map['digest']));
+
+  @override
+  List<Object?> get props => [number, parentHash, digest];
+
+  @override
+  bool? get stringify => true;
 }
 
 abstract class RpcBlock {
@@ -28,7 +40,7 @@ abstract class RpcBlock {
   const RpcBlock({this.header, required this.extrinsics});
 }
 
-class RawBlock extends RpcBlock {
+class RawBlock extends RpcBlock implements Equatable {
   final int blockNumber;
   const RawBlock(
       {required this.blockNumber,
@@ -36,11 +48,17 @@ class RawBlock extends RpcBlock {
       required List<String> extrinsics})
       : super(header: header, extrinsics: extrinsics);
 
-  static RawBlock fromMap(Map<String, dynamic> map) => RawBlock(
+  static RawBlock fromJson(Map<String, dynamic> map) => RawBlock(
         blockNumber: map['blockNumber'],
         header: map['header'] == null
             ? null
-            : RpcBlockHeader.fromMap(map['header']),
+            : RpcBlockHeader.fromJson(map['header']),
         extrinsics: (map['extrinsics'] as List).cast<String>(),
       );
+
+  @override
+  List<Object?> get props => [blockNumber, header, extrinsics];
+
+  @override
+  bool? get stringify => true;
 }
