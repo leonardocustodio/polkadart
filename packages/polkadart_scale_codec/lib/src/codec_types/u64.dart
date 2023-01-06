1b4bd956f7dd70bd720ba54ff7fa497fe1912399
Kawaljeet Singh <kawal741536@gmail.com>
2023-01-07 03:26:23 +0530

    u64 test cases, Resolve u64 - New Approach #193

--- packages/polkadart_scale_codec/lib/src/codec_types/u64.dart
+++ packages/polkadart_scale_codec/lib/src/codec_types/u64.dart
@@ -10,21 +10,58 @@ class U64 extends Codec<BigInt> {
   ///
   /// Decode a unsigned 64 bit integer from the source
   ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<BigInt>().createTypeCodec('U64', data: Source('0x0000000000000000'));
+  /// final value = codec.decode();
+  /// print(value); // 0
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<BigInt>().createTypeCodec('U64', data: Source('0x0000000000000001'));
+  /// final value = codec.decode();
+  /// print(value); // 1
+  /// ```
   @override
   BigInt decode() {
-    final low = createTypeCodec('U32').decode();
-    final high = createTypeCodec('U32').decode();
+    final low = super.createTypeCodec('U32', data: data).decode();
+    final high = super.createTypeCodec('U32', data: data).decode();
     return BigInt.from(low) + (BigInt.from(high) << 32);
   }
 
   ///
   /// Encodes a unsigned 64 bit integer
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<BigInt>().createTypeCodec('U64');
+  /// final value = codec.encode(BigInt.from(0));
+  /// print(value); // 0000000000000000
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<BigInt>().createTypeCodec('U64');
+  /// final value = codec.encode(BigInt.from(1));
+  /// print(value); // 0000000000000001
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<BigInt>().createTypeCodec('U64');
+  /// final value = codec.encode(BigInt.parse('18446744073709551615'));
+  /// print(value); // ffffffffffffffff
+  /// ```
   @override
   String encode(BigInt value) {
-    if (value >= 0 && value <= 4294967295) {
-      return encodeHex(littleEndianIntToBytes(value, 4));
+    if (value.toInt() >= 0 && value <= BigInt.parse('18446744073709551615')) {
+      final low = super.createTypeCodec('U32');
+      final high = super.createTypeCodec('U32');
+      return low.encode((value & BigInt.from(0xffffffff)).toInt()) +
+          high.encode((value >> 32).toInt());
     }
     throw UnexpectedCaseException(
-        'Expected value between 0 and 4294967295, but found: $value');
+        'Expected value between 0 and BigInt.parse("18446744073709551615"), but found: $value');
   }
 }
---
+++ packages/polkadart_scale_codec/test/codec/u64_test.dart
@@ -0,0 +1,130 @@
+import 'package:polkadart_scale_codec/polkadart_scale_codec.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('U64 Decode Test', () {
+    final registry = TypeRegistry.createRegistry();
+    test('When lowest value 0x0000000000000000 is decoded then it returns 0',
+        () {
+      final codec = Codec<BigInt>(registry: registry)
+          .createTypeCodec('u64', data: Source('0x0000000000000000'));
+      final BigInt u64Value = codec.decode();
+      expect(u64Value.toString(), equals('0'));
+    });
+
+    test(
+        'When highest value 0xffffffffffffffff is decoded then it returns 18446744073709551615',
+        () {
+      final codec = Codec<BigInt>(registry: registry)
+          .createTypeCodec('u64', data: Source('0xffffffffffffffff'));
+      final BigInt u64Value = codec.decode();
+      expect(u64Value.toString(), equals('18446744073709551615'));
+    });
+  });
+
+  group('U64 Encode Test', () {
+    final registry = TypeRegistry.createRegistry();
+    test('When lowest value 0 is encoded then it returns 0x0000000000000000',
+        () {
+      final codec = Codec<BigInt>(registry: registry).createTypeCodec('u64');
+      final String u64Value = codec.encode(BigInt.from(0));
+      expect(u64Value, equals('0000000000000000'));
+    });
+
+    test(
+        'When highest value 18446744073709551615 is encoded then it returns 0xffffffffffffffff',
+        () {
+      final codec = Codec<BigInt>(registry: registry).createTypeCodec('u64');
+      final String u64Value =
+          codec.encode(BigInt.parse('18446744073709551615'));
+      expect(u64Value, equals('ffffffffffffffff'));
+    });
+  });
+
+  group('Exception Test', () {
+    final registry = TypeRegistry.createRegistry();
+
+    test('When value -1 is encoded then it throws an exception', () {
+      final codec = Codec<BigInt>(registry: registry).createTypeCodec('u64');
+      expect(() => codec.encode(BigInt.from(-1)),
+          throwsA(isA<UnexpectedCaseException>()));
+    });
+
+    test(
+        'When value 18446744073709551616 is encoded then it throws an exception',
+        () {
+      final codec = Codec<BigInt>(registry: registry).createTypeCodec('u64');
+      expect(() => codec.encode(BigInt.parse('18446744073709551616')),
+          throwsA(isA<UnexpectedCaseException>()));
+    });
+  });
+
+  group('Custom Json Test', () {
+    final registry = TypeRegistry.createRegistry();
+    TypeRegistry.addCustomCodec(
+      registry,
+      <String, dynamic>{
+        'u64_key': 'u64',
+      },
+    );
+
+    test('When 0x0000000000000000 is decoded then it returns 0', () {
+      final codec = Codec<BigInt>(registry: registry)
+          .createTypeCodec('u64_key', data: Source('0x0000000000000000'));
+      final BigInt u64Value = codec.decode();
+      expect(u64Value.toString(), equals('0'));
+    });
+
+    test('When BigInt 0 is encoded then it returns 0x0000000000000000', () {
+      final codec =
+          Codec<BigInt>(registry: registry).createTypeCodec('u64_key');
+      final String u64Value = codec.encode(BigInt.from(0));
+      expect(u64Value, equals('0000000000000000'));
+    });
+
+    test(
+        'When 0xffffffffffffffff is decoded then it returns 18446744073709551615',
+        () {
+      final codec = Codec<BigInt>(registry: registry)
+          .createTypeCodec('u64_key', data: Source('0xffffffffffffffff'));
+      final BigInt u64Value = codec.decode();
+      expect(u64Value.toString(), equals('18446744073709551615'));
+    });
+
+    test(
+        'When BigInt 18446744073709551615 is encoded then it returns 0xffffffffffffffff',
+        () {
+      final codec =
+          Codec<BigInt>(registry: registry).createTypeCodec('u64_key');
+      final String u64Value =
+          codec.encode(BigInt.parse('18446744073709551615'));
+      expect(u64Value, equals('ffffffffffffffff'));
+    });
+  });
+
+  group('Custom Json Exception Test', () {
+    final registry = TypeRegistry.createRegistry();
+    TypeRegistry.addCustomCodec(
+      registry,
+      <String, dynamic>{
+        'u64_key': 'u64',
+      },
+    );
+
+    test('When value -1 is encoded then it throws an exception', () {
+      final codec =
+          Codec<BigInt>(registry: registry).createTypeCodec('u64_key');
+      expect(() => codec.encode(BigInt.from(-1)),
+          throwsA(isA<UnexpectedCaseException>()));
+    });
+
+    test(
+        'When value 18446744073709551616 is encoded then it throws an exception',
+        () {
+      final codec =
+          Codec<BigInt>(registry: registry).createTypeCodec('u64_key');
+      expect(() => codec.encode(BigInt.parse('18446744073709551616')),
+          throwsA(isA<UnexpectedCaseException>()));
+    });
+  });
+}
