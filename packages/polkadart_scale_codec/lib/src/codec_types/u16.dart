dc07a688b1d0bdf28199b05d9287efce1ad1b137
Kawaljeet Singh <kawal741536@gmail.com>
2023-01-07 02:17:32 +0530

    add u16 and u16 test cases

---
+++ packages/polkadart_scale_codec/lib/src/codec_types/u16.dart
@@ -0,0 +1,62 @@
+part of codec_types;
+
+///
+/// U16 to encode/decode unsigned 16 bit integer
+class U16 extends Codec<int> {
+  ///
+  /// constructor
+  U16({Registry? registry}) : super(registry: registry ?? Registry());
+
+  ///
+  /// Decode a unsigned 16 bit integer from the source
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<int>().createTypeCodec('U16', data: Source('0x0001'));
+  /// final value = codec.decode();
+  /// print(value); // 1
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<int>().createTypeCodec('U16', data: Source('0x0000'));
+  /// final value = codec.decode();
+  /// print(value); // 0
+  /// ```
+  @override
+  int decode() {
+    return bytesToLittleEndianInt(data.bytes(2).toList());
+  }
+
+  ///
+  /// Encodes a unsigned 16 bit integer
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<int>().createTypeCodec('U16');
+  /// final value = codec.encode(1);
+  /// print(value); // 0001
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<int>().createTypeCodec('U16');
+  /// final value = codec.encode(0);
+  /// print(value); // 0000
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final codec = Codec<int>().createTypeCodec('U16');
+  /// final value = codec.encode(65535);
+  /// print(value); // ffff
+  /// ```
+  @override
+  String encode(int value) {
+    if (value >= 0 && value <= 65535) {
+      return encodeHex(littleEndianIntToBytes(value, 2));
+    }
+    throw UnexpectedCaseException(
+        'Expected value between 0 and 65535, but found: $value');
+  }
+}
---
+++ packages/polkadart_scale_codec/test/codec/u16_test.dart
@@ -0,0 +1,108 @@
+import 'package:polkadart_scale_codec/polkadart_scale_codec.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('U16 Decode Test', () {
+    final registry = TypeRegistry.createRegistry();
+    test('When lowest value 0x0000 is decoded then it returns 0', () {
+      final codec = Codec(registry: registry)
+          .createTypeCodec('u16', data: Source('0x0000'));
+      final u16Value = codec.decode();
+      expect(u16Value, equals(0));
+    });
+
+    test('When highest value 0xff00 is decoded then it returns 65535', () {
+      final codec = Codec(registry: registry)
+          .createTypeCodec('u16', data: Source('0xffff'));
+      final u16Value = codec.decode();
+      expect(u16Value, equals(65535));
+    });
+  });
+
+  group('U16 Encode Test', () {
+    final registry = TypeRegistry.createRegistry();
+    test('When lowest value 0 is encoded then it returns 0x0000', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16');
+      final u16Value = codec.encode(0);
+      expect(u16Value, equals('0000'));
+    });
+
+    test('When highest value 65535 is encoded then it returns 0xffff', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16');
+      final u16Value = codec.encode(65535);
+      expect(u16Value, equals('ffff'));
+    });
+  });
+
+  group('Exception Test', () {
+    final registry = TypeRegistry.createRegistry();
+
+    test('When value -1 is encoded then it throws an exception', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16');
+      expect(() => codec.encode(-1), throwsA(isA<UnexpectedCaseException>()));
+    });
+
+    test('When value 65536 is encoded then it throws an exception', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16');
+      expect(
+          () => codec.encode(65536), throwsA(isA<UnexpectedCaseException>()));
+    });
+  });
+
+  group('Custom Json Test', () {
+    final registry = TypeRegistry.createRegistry();
+    TypeRegistry.addCustomCodec(
+      registry,
+      <String, dynamic>{
+        'u16_key': 'u16',
+      },
+    );
+
+    test('When lowest value 0x0000 is decoded then it returns 0', () {
+      final codec = Codec(registry: registry)
+          .createTypeCodec('u16_key', data: Source('0x0000'));
+      final u16Value = codec.decode();
+      expect(u16Value, equals(0));
+    });
+
+    test('When highest value 0xff00 is decoded then it returns 65535', () {
+      final codec = Codec(registry: registry)
+          .createTypeCodec('u16_key', data: Source('0xffff'));
+      final u16Value = codec.decode();
+      expect(u16Value, equals(65535));
+    });
+
+    test('When lowest value 0 is encoded then it returns 0x0000', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16_key');
+      final u16Value = codec.encode(0);
+      expect(u16Value, equals('0000'));
+    });
+
+    test('When highest value 65535 is encoded then it returns 0xffff', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16_key');
+      final u16Value = codec.encode(65535);
+      expect(u16Value, equals('ffff'));
+    });
+  });
+
+  group('Exception Test for custom json', () {
+    final registry = TypeRegistry.createRegistry();
+    TypeRegistry.addCustomCodec(
+      registry,
+      <String, dynamic>{
+        'u16_key': 'u16',
+      },
+    );
+
+    test('When value -1 is encoded then it throws an exception', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16_key');
+      expect(() => codec.encode(-1), throwsA(isA<UnexpectedCaseException>()));
+    });
+
+    test('When value 65536 is encoded then it throws an exception', () {
+      final codec = Codec<int>(registry: registry).createTypeCodec('u16_key');
+      expect(
+          () => codec.encode(65536), throwsA(isA<UnexpectedCaseException>()));
+    });
+  });
+}
