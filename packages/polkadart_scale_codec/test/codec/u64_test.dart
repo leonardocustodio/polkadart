81854f24c63956ecd9aec7c0fdbe5434a3d0dee3
Kawaljeet Singh <kawal741536@gmail.com>
2023-01-09 00:55:49 +0530

    direct test cases

--- packages/polkadart_scale_codec/test/codec/u64_test.dart
+++ packages/polkadart_scale_codec/test/codec/u64_test.dart
@@ -127,4 +127,48 @@ void main() {
           throwsA(isA<UnexpectedCaseException>()));
     });
   });
+
+  // Direct test-cases for U64(source: Source())
+  group('U64 Direct Test', () {
+    test('When lowest value 0x0000000000000000 is decoded then it returns 0',
+        () {
+      final u64Codec = U64(source: Source('0x0000000000000000'));
+      final BigInt u64Value = u64Codec.decode();
+      expect(u64Value.toString(), equals('0'));
+    });
+
+    test(
+        'When highest value 0xffffffffffffffff is decoded then it returns 18446744073709551615',
+        () {
+      final u64Codec = U64(source: Source('0xffffffffffffffff'));
+      final BigInt u64Value = u64Codec.decode();
+      expect(u64Value.toString(), equals('18446744073709551615'));
+    });
+    test('When lowest value 0 is encoded then it returns 0x0000000000000000',
+        () {
+      final u64Codec = U64();
+      final String u64Value = u64Codec.encode(BigInt.from(0));
+      expect(u64Value, equals('0000000000000000'));
+    });
+    test(
+        'When highest value 18446744073709551615 is encoded then it returns 0xffffffffffffffff',
+        () {
+      final u64Codec = U64();
+      final String u64Value =
+          u64Codec.encode(BigInt.parse('18446744073709551615'));
+      expect(u64Value, equals('ffffffffffffffff'));
+    });
+    test('When value -1 is encoded then it throws an exception', () {
+      final u64Codec = U64();
+      expect(() => u64Codec.encode(BigInt.from(-1)),
+          throwsA(isA<UnexpectedCaseException>()));
+    });
+    test(
+        'When value 18446744073709551616 is encoded then it throws an exception',
+        () {
+      final u64Codec = U64();
+      expect(() => u64Codec.encode(BigInt.parse('18446744073709551616')),
+          throwsA(isA<UnexpectedCaseException>()));
+    });
+  });
 }
