fec86aac9e9f7c822dce37f8f3e51f237152334d
Kawaljeet Singh <kawal741536@gmail.com>
2023-01-21 16:14:54 +0530

    static methods and fix tests

--- packages/polkadart_scale_codec/lib/src/codec_types/i8.dart
+++ packages/polkadart_scale_codec/lib/src/codec_types/i8.dart
@@ -3,31 +3,47 @@ part of codec_types;
 ///
 /// I8 to encode/decode signed 8 bit integer
 class I8 extends Codec<int> {
-  final Source? source;
-
   ///
   /// constructor
-  I8({this.source}) : super(registry: Registry());
+  I8._() : super(registry: Registry());
 
   ///
-  /// Decode a signed 8 bit integer from the source
+  /// Decode a signed 8 bit integer from the input
   ///
   /// Example:
   /// ```dart
-  /// final codec = Codec<int>().createTypeCodec('I8', data: Source('0x80'));
+  /// final codec = Codec<int>().createTypeCodec('I8', input: Input('0x80'));
   /// final value = codec.decode();
   /// print(value); // -128
   /// ```
   ///
   /// Example:
   /// ```dart
-  /// final codec = Codec<int>().createTypeCodec('I8', data: Source('0x7f'));
+  /// final codec = Codec<int>().createTypeCodec('I8', input: Input('0x7f'));
   /// final value = codec.decode();
   /// print(value); // 127
   /// ```
   @override
   int decode() {
-    final byte = (source ?? data).byte();
+    return decodeFromInput(input);
+  }
+
+  ///
+  /// [static] Decode a signed 8 bit integer from the input
+  ///
+  /// Example:
+  /// ```dart
+  /// final value = I8.decode(input: Input('0x80'));
+  /// print(value); // -128
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final value = I8.decode(input: Input('0x7f'));
+  /// print(value); // 127
+  /// ```
+  static int decodeFromInput(Input input) {
+    final byte = input.byte();
     return (byte | (byte & 0x80) * 0x1fffffe).toSigned(16);
   }
 
@@ -48,12 +64,32 @@ class I8 extends Codec<int> {
   /// print(value); // 7f
   /// ```
   @override
-  String encode(int value) {
+  void encode(Encoder encoder, int value) {
+    encodeTo(encoder, value);
+  }
+
+  ///
+  /// [static] Encodes a signed 8 bit integer
+  ///
+  /// Example:
+  /// ```dart
+  /// final encoder = HexEncoder();
+  /// final value = I8.encodeTo(encoder, -128);
+  /// print(encoder.toHex()); // 80
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final encoder = HexEncoder();
+  /// final value = I8.encodeTo(encoder, 127);
+  /// print(encoder.toHex()); // 7f
+  /// ```
+  static void encodeTo(Encoder encoder, int value) {
     if (value < -128 || value > 127) {
       throw UnexpectedCaseException(
           'Expected value between -128 and 127, but found: $value');
     }
 
-    return encodeHex([(value + 256) % 256]);
+    encoder.write((value + 256) % 256);
   }
 }
