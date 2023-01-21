ca7034501c93fd8d7bf3c369d4fbcc871b358b31
Kawaljeet Singh <kawal741536@gmail.com>
2023-01-21 16:23:15 +0530

    static methods and test cases

--- packages/polkadart_scale_codec/lib/src/codec_types/i16.dart
+++ packages/polkadart_scale_codec/lib/src/codec_types/i16.dart
@@ -3,32 +3,47 @@ part of codec_types;
 ///
 /// I16 to encode/decode signed 16 bit integer
 class I16 extends Codec<int> {
-  final Source? source;
-
   ///
   /// constructor
-  I16({Registry? registry, this.source})
-      : super(registry: registry ?? Registry());
+  I16._() : super(registry: Registry());
 
   ///
   /// Decode a signed 16 bit integer from the source
   ///
   /// Example:
   /// ```dart
-  /// final codec = Codec<int>().createTypeCodec('I16', data: Source('0x0080'));
+  /// final codec = Codec<int>().createTypeCodec('I16', input: Input('0x0080'));
   /// final value = codec.decode();
   /// print(value); // -32768
   /// ```
   ///
   /// Example:
   /// ```dart
-  /// final codec = Codec<int>().createTypeCodec('I16', data: Source('0xff7f'));
+  /// final codec = Codec<int>().createTypeCodec('I16', input: Input('0xff7f'));
   /// final value = codec.decode();
   /// print(value); // 32767
   /// ```
   @override
   int decode() {
-    final value = bytesToLittleEndianInt((source ?? data).bytes(2).toList());
+    return decodeFromInput(input);
+  }
+
+  ///
+  /// [static] Decode a signed 16 bit integer from the input
+  ///
+  /// Example:
+  /// ```dart
+  /// final value = I16.decodeFromInput(input: Input('0x0080'));
+  /// print(value); // -32768
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final value = I16.decodeFromInput(input: Input('0xff7f'));
+  /// print(value); // 32767
+  /// ```
+  static int decodeFromInput(Input input) {
+    final value = bytesToLittleEndianInt(input.bytes(2).toList());
     return (value | (value & (1 << 15)) * 0x1fffe).toSigned(16);
   }
 
@@ -49,13 +64,33 @@ class I16 extends Codec<int> {
   /// print(value); // ff7f
   /// ```
   @override
-  String encode(int value) {
+  void encode(Encoder encoder, int value) {
+    encodeTo(encoder, value);
+  }
+
+  ///
+  /// [static] Encodes a signed 16 bit integer
+  ///
+  /// Example:
+  /// ```dart
+  /// final encoder = HexEncoder();
+  /// final value = I16.encodeToBytes(encoder, -32768);
+  /// print(encoder.toHex()); // 0080
+  /// ```
+  ///
+  /// Example:
+  /// ```dart
+  /// final encoder = HexEncoder();
+  /// final value = I16.encodeToBytes(encoder, 32767);
+  /// print(encoder.toHex()); // ff7f
+  /// ```
+  static void encodeTo(Encoder encoder, int value) {
     if (value < -32768 || value > 32767) {
       throw UnexpectedCaseException(
           'Expected value between -32768 and 32767, but found: $value');
     }
 
     final base = (value + 65536) % 65536;
-    return encodeHex(<int>[base & 0xff, base >>> 8]);
+    encoder.writeBytes(<int>[base & 0xff, base >>> 8]);
   }
 }
