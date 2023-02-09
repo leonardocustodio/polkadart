a1b9bd3a00ce95765bb0de37bfd315cb5612324b
Kawaljeet Singh <kawal741536@gmail.com>
2023-02-09 19:58:38 +0530

    enum extensions

---
+++ packages/polkadart_scale_codec/lib/utils/enum_extensions.dart
@@ -0,0 +1,22 @@
+part of utils;
+
+///
+/// Usage:
+/// ```dart
+/// enum TestEnum { a, b, c }
+///
+/// void main() {
+///   final output = HexOutput();
+///
+///   TestEnum.b.encodeTo(output);
+///
+///   print(output.toString()); // 0x00
+/// }
+/// ```
+extension EnumExtensions<T extends core.Enum> on T {
+  ///
+  /// Encodes value of Enum to the [HexOutput] / [ByteOutput]
+  void encodeTo(Output output) {
+    output.pushByte(index);
+  }
+}
--- packages/polkadart_scale_codec/lib/utils/utils.dart
+++ packages/polkadart_scale_codec/lib/utils/utils.dart
@@ -2,7 +2,11 @@ library utils;
 
 import 'dart:typed_data';
 import 'package:convert/convert.dart';
+import 'dart:core' as core;
+import 'dart:core';
+import 'package:polkadart_scale_codec/io/io.dart';
 
 part 'exceptions.dart';
 part 'functions.dart';
 part 'regexp.dart';
+part 'enum_extensions.dart';
