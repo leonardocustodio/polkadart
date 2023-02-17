e18398098cf248e17e70513fa1dc0c7739c1bd29
Kawaljeet Singh <kawal741536@gmail.com>
2023-02-18 00:43:04 +0530

    fix test

--- packages/substrate_metadata/test/metadata_tests/metadata_decode_test.dart
+++ packages/substrate_metadata/test/metadata_tests/metadata_decode_test.dart
@@ -2,29 +2,33 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:polkadart_scale_codec/io/io.dart';
-import 'package:polkadart_scale_codec/utils/utils.dart';
 import 'package:substrate_metadata/substrate_metadata.dart';
+import 'package:test/test.dart';
 
 void main() {
-  final versions = [12, 13, 14];
-  for (final version in versions) {
-    final String metadataFilePath =
-        '../substrate_metadata/test/metadata_tests/metadata.json';
-    final String fileContents = File(metadataFilePath).readAsStringSync();
+  group('Metadata Tests:', () {
+    final versions = [12, 13, 14];
+    for (final version in versions) {
+      test('Decode/Encode V$version', () {
+        final String metadataFilePath =
+            '../substrate_metadata/test/metadata_tests/metadata.json';
+        final String fileContents = File(metadataFilePath).readAsStringSync();
 
-    final Map<String, dynamic> metadata = jsonDecode(fileContents);
-    final String metadataHex = metadata['v$version']!;
+        final Map<String, dynamic> metadata = jsonDecode(fileContents);
+        final String metadataHex = metadata['v$version']!;
 
-    // decoding
-    final input = HexInput(metadataHex);
+        // decoding
+        final input = HexInput(metadataHex);
 
-    final decodedMetadata = MetadataDecoder.instance.decode(input);
+        final decodedMetadata = MetadataDecoder.instance.decode(input);
 
-    final output = HexOutput();
-    MetadataDecoder.instance.encode(decodedMetadata, output);
+        final output = HexOutput();
+        MetadataDecoder.instance.encode(decodedMetadata, output);
 
-    final encodedMetadataHex = output.toString();
+        final encodedMetadataHex = output.toString();
 
-    assertion(encodedMetadataHex == metadataHex);
-  }
+        expect(encodedMetadataHex, metadataHex);
+      });
+    }
+  });
 }
