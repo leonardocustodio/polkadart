af93ff6e2a6fcd26ede0fb4f655cbf6aea30096d
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-03-07 03:10:32 -0300

    Remove unused code

--- packages/codec_generator/lib/generator_types/generator_base.dart
+++
@@ -1,8 +0,0 @@
-library generator_types;
-
-import 'package:codec_generator/exceptions.dart';
-import 'package:utility/utility.dart';
-
-part 'primitive_type_generator.dart';
-part 'type_generator.dart';
-part 'type_fields.dart';
