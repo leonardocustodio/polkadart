1cfd7f2cdbfb758e79a5ee0f062d4529d635bb70
Kawaljeet Singh <kawal741536@gmail.com>
2023-03-16 23:12:03 +0530

    move files to script folder

--- scripts/run_dart_pub_get.sh
+++ scripts/run_dart_pub_get.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd packages
+cd ../packages
 # executes by going in every sub directory inside packages up to 2 levels deep until found `pubspec.yaml` and runs dart pub get
 find . -maxdepth 2 -name pubspec.yaml -execdir dart pub get \;
