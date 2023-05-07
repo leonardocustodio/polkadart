4612344fb2bbd2d0d65baf4c9eeb1007cb53154d
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-05-07 17:35:39 -0300

    Configure FVM

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,8 +3,8 @@
 # Fast fail the script on failures.
 set -e
 
-dart pub global activate coverage
+fvm dart pub global activate coverage
 
-dart test --coverage="coverage"
+fvm dart test --coverage="coverage"
 
-format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
+fvm dart run coverage:format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
