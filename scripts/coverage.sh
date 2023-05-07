085849f901e9ad561ac2ee16d719fd5ead6beb9a
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-05-07 17:55:52 -0300

    Install coverage tool locally instead of globaly

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,8 +3,6 @@
 # Fast fail the script on failures.
 set -e
 
-fvm dart pub global activate coverage
-
 fvm dart test --coverage="coverage"
 
 fvm dart run coverage:format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
