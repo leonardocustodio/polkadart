23ff63bcfd586ea6a49e268d2be0a06c123d2c4e
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-07-10 04:17:20 -0300

    Support substrate BIP39 for Ed25519

---
+++ packages/substrate_bip39/test/secret_uri_test.dart
@@ -0,0 +1,63 @@
+import 'package:substrate_bip39/secret_uri.dart' show SecretUri, DeriveJunction;
+import 'package:test/test.dart';
+
+void main() {
+  test('interpret std secret string', () async {
+    expect(
+        SecretUri.fromStr('hello world'), SecretUri('hello world', null, []));
+
+    expect(SecretUri.fromStr('hello world/1'),
+        SecretUri('hello world', null, [DeriveJunction.fromStr('1')]));
+
+    expect(SecretUri.fromStr('hello world/DOT'),
+        SecretUri('hello world', null, [DeriveJunction.fromStr('DOT')]));
+
+    expect(
+        SecretUri.fromStr(
+            'hello world/0123456789012345678901234567890123456789'),
+        SecretUri('hello world', null, [
+          DeriveJunction.fromStr('0123456789012345678901234567890123456789')
+        ]));
+
+    expect(SecretUri.fromStr('hello world//1'),
+        SecretUri('hello world', null, [DeriveJunction.fromStr('/1')]));
+
+    expect(SecretUri.fromStr('hello world//DOT'),
+        SecretUri('hello world', null, [DeriveJunction.fromStr('/DOT')]));
+
+    expect(
+        SecretUri.fromStr(
+            'hello world//0123456789012345678901234567890123456789'),
+        SecretUri('hello world', null, [
+          DeriveJunction.fromStr('/0123456789012345678901234567890123456789')
+        ]));
+
+    expect(
+        SecretUri.fromStr('hello world//1/DOT'),
+        SecretUri('hello world', null, [
+          DeriveJunction.fromStr('/1'),
+          DeriveJunction.fromStr('DOT'),
+        ]));
+
+    expect(
+        SecretUri.fromStr('hello world//DOT/1'),
+        SecretUri('hello world', null, [
+          DeriveJunction.fromStr('/DOT'),
+          DeriveJunction.fromStr('1'),
+        ]));
+
+    expect(
+        SecretUri.fromStr('hello world//1/DOT///password'),
+        SecretUri('hello world', 'password', [
+          DeriveJunction.fromStr('/1'),
+          DeriveJunction.fromStr('DOT'),
+        ]));
+
+    expect(
+        SecretUri.fromStr('hello world/1//DOT///password'),
+        SecretUri('hello world', 'password', [
+          DeriveJunction.fromStr('1'),
+          DeriveJunction.fromStr('/DOT'),
+        ]));
+  });
+}
