cadf07818a9d8ce992630041374d13d92dbb2e7c
Leonardo Custodio <leonardo@enjin.io>
2023-10-09 16:57:25 -0300

    Add submitExtrinsic

--- packages/polkadart/lib/apis/apis.dart
+++ packages/polkadart/lib/apis/apis.dart
@@ -3,6 +3,7 @@ library apis;
 import 'dart:typed_data' show Uint8List;
 import 'dart:async' show Future, StreamSubscription;
 import 'package:convert/convert.dart' show hex;
+import 'package:substrate_metadata/utils/utils.dart';
 import '../primitives/primitives.dart'
     show
         BlockHash,
@@ -19,5 +20,6 @@ import '../primitives/primitives.dart'
         ReadProof;
 import '../../provider.dart' show Provider;
 
+part './author.dart';
 part './state.dart';
 part './system.dart';
