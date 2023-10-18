0f42065561249fdbf3dc3f7ea05fe09dcc865820
Leonardo Custodio <leonardo@custodio.me>
2023-10-18 20:42:21 -0300

    Removes git-lfs from repository (#340)
    * remove git attr
    
    * Add files under 50mb
    
    * Split events and blocks
    
    * Optimize memory
    
    * remove empty line

--- packages/substrate_metadata/test/chains/kusama/events_test.dart
+++ packages/substrate_metadata/test/chains/kusama/events_test.dart
@@ -21,34 +21,36 @@ void main() {
     // Populating with the metadata for block-numbers available for this chain....
     chain.initSpecVersionFromFile('../../chain/kusama/versions.jsonl');
 
-    final List<RawBlockEvents> rawBlocksList =
-        RawBlockEvents.readEventsFromPath('../../chain/kusama/events.jsonl');
+    void parseEventList(List<RawBlockEvents> rawEventList) {
+      for (var originalEvent in rawEventList) {
+        test(
+            'When original event is decoded and encoded back then it matches the original event value.',
+            () {
+          //
+          // Decoding the `Raw Block Events`
+          final decodedBlockEvents = chain.decodeEvents(originalEvent);
 
-    //
-    // Looping through every block
-    for (var originalEvent in rawBlocksList) {
-      test(
-          'When original event is decoded and encoded back then it matches the original event value.',
-          () {
-        //
-        // Decoding the `Raw Block Events`
-        final decodedBlockEvents = chain.decodeEvents(originalEvent);
+          //
+          // Encoding the `Decoded Block Events`
+          final encodedBlockEvents = chain.encodeEvents(decodedBlockEvents);
 
-        //
-        // Encoding the `Decoded Block Events`
-        final encodedBlockEvents = chain.encodeEvents(decodedBlockEvents);
+          //
+          // Comparing the original event with the encoded event
+          expect(originalEvent.events, encodedBlockEvents.events);
 
-        //
-        // Comparing the original event with the encoded event
-        expect(originalEvent.events, encodedBlockEvents.events);
+          final againDecodedEvents = chain.decodeEvents(encodedBlockEvents);
 
-        final againDecodedEvents = chain.decodeEvents(encodedBlockEvents);
-
-        //
-        // Comparing the decoded event with the decodedFromEncoded event
-        expect(decodedBlockEvents.events.toJson().toString(),
-            againDecodedEvents.events.toJson().toString());
-      });
+          //
+          // Comparing the decoded event with the decodedFromEncoded event
+          expect(decodedBlockEvents.events.toJson().toString(),
+              againDecodedEvents.events.toJson().toString());
+        });
+      }
     }
+
+    parseEventList(RawBlockEvents.readEventsFromPath(
+        '../../chain/kusama/events.part1.jsonl'));
+    parseEventList(RawBlockEvents.readEventsFromPath(
+        '../../chain/kusama/events.part2.jsonl'));
   });
 }
