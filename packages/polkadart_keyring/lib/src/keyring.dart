e99f676ad2dd1353da42156ef3b7ca4b6d229293
Kawaljeet Singh <kawal741536@gmail.com>
2023-10-25 21:47:24 -0400

    add test cases for pairs_test

--- packages/polkadart_keyring/lib/src/keyring.dart
+++ packages/polkadart_keyring/lib/src/keyring.dart
@@ -2,13 +2,31 @@ import 'package:polkadart_keyring/src/pairs.dart';
 
 import '../polkadart_keyring.dart';
 
+/// A manager for ed25519-based key pairs in the keyring.
 ///
-/// Creates a new Keyring object for ed25519-based key pairs.
+/// The `Keyring` class provides a convenient interface to manage ed25519-based key pairs.
+/// It allows you to create key pairs from BIP39 mnemonics, add them to the keyring,
+/// retrieve key pairs by address or public key, and perform various operations on them.
+///
+/// Example usages for each function are provided below.
 class Keyring {
   final Pairs pairs;
 
   Keyring() : pairs = Pairs();
 
+  /// Create a new [KeyPair] from a BIP39 mnemonic and optionally add it to the keyring.
+  ///
+  /// This method generates a key pair from the provided [mnemonic].
+  ///
+  /// - [mnemonic]: The BIP39 mnemonic phrase used to derive the key pair.
+  /// - [addToPairs]: If set to `true`, the key pair will be added to the keyring.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final mnemonic = "your mnemonic phrase";
+  /// final keyPair = await keyring.createKeyPairFromMnemonic(mnemonic, addToPairs: true);
+  /// ```
   Future<KeyPair> createKeyPairFromMnemonic(String mnemonic,
       {bool addToPairs = false}) async {
     final pair = await KeyPair.fromMnemonic(mnemonic);
@@ -18,45 +36,94 @@ class Keyring {
     return pair;
   }
 
-  ///
   /// Add a [KeyPair] to the keyring.
+  ///
+  /// - [pair]: The key pair to be added to the keyring.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final keyPair = KeyPair(); // Create or obtain a KeyPair
+  /// keyring.add(keyPair);
+  /// ```
   void add(KeyPair pair) {
     pairs.add(pair);
   }
 
-  ///
   /// Get all [KeyPair]s in the keyring.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final allKeyPairs = keyring.all;
+  /// ```
   List<KeyPair> get all {
     return pairs.all;
   }
 
-  ///
-  /// Get a [KeyPair] from the keyring by [address]
+  /// Get a [KeyPair] from the keyring by address.
   /// If the [pair] does not exist, it will throw an error.
+  ///
+  /// - [address]: The address of the key pair to retrieve.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final address = "your_address";
+  /// final keyPair = keyring.getByAddress(address);
+  /// ```
   KeyPair getByAddress(String address) {
     return pairs.getByAddress(address);
   }
 
+  /// Get a [KeyPair] from the keyring by public key.
+  ///
+  /// - [publicKey]: The public key (as a list of integers) of the key pair to retrieve.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final publicKey = [1, 2, 3]; // Replace with an actual public key
+  /// final keyPair = keyring.getByPublicKey(publicKey);
+  /// ```
   KeyPair getByPublicKey(List<int> publicKey) {
     return pairs.getByPublicKey(publicKey);
   }
 
+  /// Remove a [KeyPair] from the keyring by address.
   ///
-  /// Remove a [KeyPair] from the keyring by [address].
+  /// - [address]: The address of the key pair to remove.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final address = "your_address";
+  /// keyring.remove(address);
+  /// ```
   void remove(String address) {
     pairs.remove(address);
   }
 
-  ///
   /// Get all public keys in the keyring.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final publicKeys = keyring.publicKeys;
+  /// ```
   List<List<int>> get publicKeys {
     return pairs.all
         .map((pair) => List<int>.from(pair.publicKey.bytes))
         .toList();
   }
 
-  ///
   /// Get all addresses in the keyring.
+  ///
+  /// Example:
+  /// ```dart
+  /// final keyring = Keyring();
+  /// final addresses = keyring.addresses;
+  /// ```
   List<String> get addresses {
     return pairs.all.map((pair) => pair.address).toList();
   }
--- packages/polkadart_keyring/lib/src/pairs.dart
+++ packages/polkadart_keyring/lib/src/pairs.dart
@@ -1,16 +1,29 @@
 import 'dart:typed_data';
-
 import 'package:ss58/ss58.dart';
-
 import '../polkadart_keyring.dart';
 
+/// A collection of key pairs for managing ed25519-based cryptographic operations.
+///
+/// The `Pairs` class provides a way to manage key pairs by allowing you to add, remove, and retrieve
+/// key pairs by address or public key. It also offers methods to get lists of all key pairs, public keys,
+/// and addresses within the collection.
+///
+/// Example usages and function descriptions are provided below.
 class Pairs {
   final Map<String, KeyPair> _pairs = <String, KeyPair>{};
 
+  /// Add a [KeyPair] to the collection of pairs.
+  ///
+  /// - [pair]: The key pair to add to the collection.
   void add(KeyPair pair) {
-    _pairs[Address.decode(pair.address).pubkey.toList().toString()] = pair;
+    _pairs[pair.address] = pair;
   }
 
+  /// Get a [KeyPair] from the collection by its address.
+  ///
+  /// - [address]: The address of the key pair to retrieve.
+  ///
+  /// Throws an exception if the key pair associated with the provided [address] is not found.
   KeyPair getByAddress(String address) {
     if (_pairs[address] == null) {
       throw Exception('KeyPair associated with address $address not found.');
@@ -18,38 +31,55 @@ class Pairs {
     return _pairs[address]!;
   }
 
+  /// Get a [KeyPair] from the collection by its public key.
+  ///
+  /// - [publicKey]: The public key (as a list of integers) of the key pair to retrieve.
+  ///
+  /// Example:
+  /// ```dart
+  /// final publicKey = [1, 2, 3]; // Replace with an actual public key
+  /// final keyPair = pairs.getByPublicKey(publicKey);
+  /// ```
   KeyPair getByPublicKey(List<int> publicKey) {
     final address =
         Address(prefix: 42, pubkey: Uint8List.fromList(publicKey)).encode();
     return getByAddress(address);
   }
 
+  /// Remove a [KeyPair] from the collection by its address.
   ///
-  /// Remove a [KeyPair] from the keyring by [address].
+  /// - [address]: The address of the key pair to remove.
   ///
-  /// If the [pair] exists, it will be removed.
+  /// If the key pair exists, it will be removed.
   void remove(String address) {
     _pairs.remove(address);
   }
 
+  /// Remove all [KeyPair]s from the collection.
   void removeAll() {
     _pairs.clear();
   }
 
+  /// Remove a [KeyPair] from the collection by its public key.
+  ///
+  /// - [publicKey]: The public key (as a list of integers) of the key pair to remove.
   void removeByPublicKey(List<int> publicKey) {
     final address =
         Address(prefix: 42, pubkey: Uint8List.fromList(publicKey)).encode();
     remove(address);
   }
 
+  /// Get all [KeyPair]s in the collection.
   List<KeyPair> get all {
     return List<KeyPair>.from(_pairs.values.toList());
   }
 
+  /// Get a list of all public keys from the collection.
   List<List<int>> get publicKeys {
     return all.map((pair) => pair.publicKey.bytes).toList();
   }
 
+  /// Get a list of all addresses from the collection.
   List<String> get addresses {
     return all.map((pair) => pair.address).toList();
   }
