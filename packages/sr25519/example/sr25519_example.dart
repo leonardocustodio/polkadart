c5a8a3d1e0dc6d79e1d59861d9c490df9aa6f317
Kawaljeet Singh <kawal741536@gmail.com>
2023-11-04 13:21:23 -0400

    add sr25519 package

---
+++ packages/sr25519/example/sr25519_example.dart
@@ -0,0 +1,6 @@
+import 'package:sr25519/sr25519.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/sr25519/lib/sr25519.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/sr25519_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
