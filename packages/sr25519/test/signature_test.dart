8bea94514342642ab7b4f83861b485a0bb99bc2f
Kawaljeet Singh <kawal741536@gmail.com>
2023-11-21 21:48:04 -0500

    add 'Test verify rust test case'

--- packages/sr25519/test/signature_test.dart
+++ packages/sr25519/test/signature_test.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:collection/collection.dart';
+import 'package:convert/convert.dart';
 import 'package:sr25519/sr25519.dart';
 import 'package:test/test.dart';
 import 'package:merlin/merlin.dart' as merlin;
@@ -61,7 +64,6 @@ void main() {
     expect(verified2, true);
   });
 
-
   test('Test Verify Keypair', () {
     final merlin.Transcript transcript = merlin.Transcript('hello');
     final KeyPair keypair = KeyPair.generateKeypair();
@@ -104,5 +106,25 @@ void main() {
 
     expect(const ListEquality<int>().equals(sExpected, sActual), true);
     expect(const ListEquality<int>().equals(rExpected, rActual), true);
+  });
+
+  test('Test verify rust', () {
+    final List<int> publicBytes = hex.decode(
+        '46ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a');
+
+    final PublicKey pub = PublicKey();
+    pub.decode(publicBytes);
+
+    final List<int> message = utf8.encode('this is a message');
+
+    final Signature sig = Signature.fromHex(
+        '4e172314444b8f820bb54c22e95076f220ed25373e5c178234aa6c211d29271244b947e3ff3418ff6b45fd1df1140c8cbff69fc58ee6dc96df70936a2bb74b82');
+
+    final merlin.Transcript transcript =
+        Sr25519.newSigningContext(utf8.encode('substrate'), message);
+
+    late bool verified;
+    expect(() => (verified, _) = pub.verify(sig, transcript), returnsNormally);
+    expect(verified, true);
   });
 }
