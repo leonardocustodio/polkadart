6b7f9377488bfcfc73ad618f72a4f8253e3f477a
Kawaljeet Singh <kawal741536@gmail.com>
2023-11-21 21:46:54 -0500

    add 'Test Verify Keypair'

--- packages/sr25519/test/signature_test.dart
+++ packages/sr25519/test/signature_test.dart
@@ -60,4 +60,27 @@ void main() {
     expect(verified2, true);
   });
 
+
+  test('Test Verify Keypair', () {
+    final merlin.Transcript transcript = merlin.Transcript('hello');
+    final KeyPair keypair = KeyPair.generateKeypair();
+    final (private, public) = (keypair.secretKey, keypair.publicKey);
+
+    final KeyPair kp = KeyPair.from(public, private);
+    late Signature signature;
+    expect(() => signature = kp.sign(transcript), returnsNormally);
+
+    final merlin.Transcript transcript2 = merlin.Transcript('hello');
+    late bool verified;
+    expect(() => (verified, _) = kp.verify(signature, transcript2),
+        returnsNormally);
+    expect(verified, true);
+
+    final merlin.Transcript transcript3 = merlin.Transcript('hello');
+    late bool verified2;
+    expect(() => (verified2, _) = kp.verify(signature, transcript3),
+        returnsNormally);
+    expect(verified2, true);
+  });
+
 }
