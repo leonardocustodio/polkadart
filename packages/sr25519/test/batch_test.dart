bf8bcf0a913aa8b4ff22c37040a5e5272355f745
Kawaljeet Singh <kawal741536@gmail.com>
2023-11-21 21:38:43 -0500

    add batch_test.dart

---
+++ packages/sr25519/test/batch_test.dart
@@ -0,0 +1,33 @@
+import 'package:sr25519/sr25519.dart';
+import 'package:test/test.dart';
+import 'package:merlin/merlin.dart' as merlin;
+
+void main() {
+  test('Verify Batch', () {
+    final int num = 16;
+    final List<merlin.Transcript> transcripts = <merlin.Transcript>[];
+    final List<Signature> sigs = <Signature>[];
+    final List<PublicKey> pubkeys = <PublicKey>[];
+
+    for (int i = 0; i < num; i++) {
+      final transcript = merlin.Transcript('hello_$i');
+      final keypair = KeyPair.generateKeypair();
+      final priv = keypair.secretKey;
+      final pub = keypair.publicKey;
+
+      expect(() => sigs.add(priv.sign(transcript)), returnsNormally);
+
+      expect(() => transcripts.add(merlin.Transcript('hello_$i')),
+          returnsNormally);
+
+      expect(() => pubkeys.add(pub), returnsNormally);
+    }
+
+    expect(transcripts.length, num);
+    expect(sigs.length, num);
+    expect(pubkeys.length, num);
+
+    final verified = verifyBatch(transcripts, sigs, pubkeys);
+    expect(verified, true, reason: 'failed to batch verify signatures');
+  });
+}
