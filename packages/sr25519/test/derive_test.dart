a48cfe77f59df7b948c90fa09122d42da7e5ec7f
Kawaljeet Singh <kawal741536@gmail.com>
2023-11-21 21:41:52 -0500

    add test 'Test Derive Soft'

--- packages/sr25519/test/derive_test.dart
+++ packages/sr25519/test/derive_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:collection/collection.dart';
+import 'package:convert/convert.dart';
 import 'package:cryptography/dart.dart';
 import 'package:merlin/merlin.dart' as merlin;
 import 'package:sr25519/sr25519.dart';
@@ -54,4 +55,71 @@ void main() {
     final (verified, _) = (dpub.key as PublicKey).verify(sig, verifyTranscript);
     expect(verified, true);
   });
+
+  test('Test Derive Soft', () {
+    // test vectors from https://github.com/Warchant/sr25519-crust/blob/master/test/derive.cpp#L32
+
+    final vector = TestingVector(
+      keyPair:
+          "4c1250e05afcd79e74f6c035aee10248841090e009b6fd7ba6a98d5dc743250cafa4b32c608e3ee2ba624850b3f14c75841af84b16798bf1ee4a3875aa37a2cee661e416406384fe1ca091980958576d2bff7c461636e9f22c895f444905ea1f",
+      chainCode:
+          "0c666f6f00000000000000000000000000000000000000000000000000000000",
+      public:
+          "b21e5aabeeb35d6a1bf76226a6c65cd897016df09ef208243e59eed2401f5357",
+      hard: false,
+    );
+
+    vector.deriveHardSoft();
+  });
+}
+
+/// commonVectors is a struct to set the vectors used for deriving soft or hard
+/// keys for testing
+class TestingVector {
+  /// KeyPair in the hex encoded string of a known keypair
+  final String keyPair;
+
+  /// ChainCode is the chain code for generating the derived key hex encoded
+  final String chainCode;
+
+  /// Public is the expected resulting public key of the derived key hex
+  /// encoded
+  final String public;
+
+  /// Hard indicates if the vectors are for deriving a Hard key
+  final bool hard;
+
+  const TestingVector({
+    required this.keyPair,
+    required this.chainCode,
+    required this.public,
+    required this.hard,
+  });
+
+  /// deriveCommon provides common functions for testing Soft and Hard key derivation
+  void deriveHardSoft() {
+    final kp = hex.decode(keyPair);
+
+    final cc = hex.decode(chainCode);
+
+    final List<int> privBytes = List.from(kp.sublist(0, 32), growable: false);
+    final priv = SecretKey();
+    priv.decode(privBytes);
+    final List<int> ccBytes = List.from(cc.sublist(0, 32), growable: false);
+
+    late ExtendedKey derived;
+
+    if (hard) {
+      derived = ExtendedKey.deriveKeyHard(priv, [], ccBytes);
+    } else {
+      derived = ExtendedKey.deriveKeySimple(priv, [], ccBytes);
+    }
+
+    final List<int> expectedPub = hex.decode(public);
+
+    final PublicKey resultPub = derived.public();
+
+    final List<int> resultPubBytes = resultPub.encode();
+    expect(const ListEquality().equals(resultPubBytes, expectedPub), true);
+  }
 }
