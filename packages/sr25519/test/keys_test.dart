e107d3d8870fa8741afc346d12c5d4c04b58c84e
Kawaljeet Singh <kawal741536@gmail.com>
2023-11-21 21:43:57 -0500

    add test 'Test MiniSecret Key_ExpandEd25519'

--- packages/sr25519/test/keys_test.dart
+++ packages/sr25519/test/keys_test.dart
@@ -27,7 +27,7 @@ void main() {
 
     expect(const ListEquality().equals(actualBytes, expectedBytes), true);
   });
-  
+
   test('Test Generate Keypair From SecretKey', () {
     final keyPair = KeyPair.generateKeypair();
 
@@ -39,5 +39,25 @@ void main() {
     final actualBytes = kp.publicKey.encode();
 
     expect(const ListEquality().equals(actualBytes, expectedBytes), true);
+  });
+
+  // test cases from: https://github.com/Warchant/sr25519-crust/blob/master/test/keypair_from_seed.cpp
+  test('Test MiniSecret Key_ExpandEd25519', () {
+    final msc = MiniSecretKey.fromRawKey(List<int>.filled(32, 0));
+
+    final SecretKey sc = msc.expandEd25519();
+
+    final List<int> expectedBytes = hex.decode(
+        'caa835781b15c7706f65b71f7a58c807ab360faed6440fb23e0f4c52e930de0a0a6a85eaa642dac835424b5d7c8d637c00408c7a73da672b7f498521420b6dd3def12e42f3e487e9b14095aa8d5cc16a33491f1b50dadcf8811d1480f3fa8627');
+
+    expect(const ListEquality().equals(expectedBytes.sublist(0, 32), sc.key),
+        true);
+    expect(const ListEquality().equals(expectedBytes.sublist(32, 64), sc.nonce),
+        true);
+
+    final List<int> publicKeyBytes = msc.public().encode();
+    expect(
+        const ListEquality().equals(expectedBytes.sublist(64), publicKeyBytes),
+        true);
   });
 }
