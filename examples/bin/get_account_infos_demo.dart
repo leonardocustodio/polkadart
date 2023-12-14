6475141b92bdd81f2fc6f85a9a7f7ba47d25ed9d
clangenb <37865735+clangenb@users.noreply.github.com>
2023-12-14 08:52:38 +0100

    [examples] add `get_account_infos_example` and add examples to melos workspace

---
+++ examples/bin/get_account_infos_demo.dart
@@ -0,0 +1,27 @@
+import 'package:convert/convert.dart';
+import 'package:polkadart/scale_codec.dart';
+import 'package:polkadart_example/generated/polkadot/types/sp_core/crypto/account_id32.dart';
+import '../lib/generated/polkadot/polkadot.dart';
+import 'package:polkadart/polkadart.dart' show Provider;
+
+Future<void> main(List<String> arguments) async {
+  final provider = Provider.fromUri(Uri.parse('wss://rpc.polkadot.io'));
+  final polkadot = Polkadot(provider);
+
+  final accountMapPrefix = polkadot.query.system.accountMapPrefix();
+  final keys = await polkadot.rpc.state.getKeysPaged(key: accountMapPrefix, count: 10);
+
+  print("First 10 account storage keys: ${keys.map((key) => '0x${hex.encode(key)}')}");
+
+  // Decoding of the keys has to be done manually for now, see how substrate storage keys are defined:
+  // https://www.shawntabrizi.com/blog/substrate/transparent-keys-in-substrate/
+  final accountIds = keys.map((key) => const AccountId32Codec().decode(ByteInput(key.sublist(32))));
+  print("First 10 account pubKeys: ${accountIds.map((account) => '0x${hex.encode(account)}')}");
+
+  // Get account data of those keys
+  final accountInfos = await Future.wait(accountIds.map((account) => polkadot.query.system.account(account)));
+
+  for (final accountInfo in accountInfos) {
+    print('AccountInfo: ${accountInfo.toJson()}');
+  }
+}
