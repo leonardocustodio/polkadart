5a946fb816e1805f3e4b3947be6d09c2a608680a
clangenb <37865735+clangenb@users.noreply.github.com>
2023-12-19 19:08:07 +0100

    [CI] fail if fmt is wrong and check `polkadart_example` (#382)
    * [CI] set exit code if files changed after format
    
    * [CI] check example in CI
    
    * fmt
    
    * [CI] generate the files for the example
    
    * [CI] fix polkadart generate
    
    * [CI] not necessary to format generated files as it seems.
    
    * [polkadart_example] add mock test as melos expects it
    
    * fmt

---
+++ examples/test/mock_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+
+void main() {
+  test(
+      'Mock test, as melos expects at least a test. Otherwise it returns an error.',
+      () {
+    expect(true, true);
+  });
+}
