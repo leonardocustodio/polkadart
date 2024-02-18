821830d0584595a95b4ddf7b068de976367decfd
Kawaljeet Singh <kawal741536@gmail.com>
2024-02-18 18:06:40 -0500

    remove multisig from polkadart_keyring

--- packages/polkadart_keyring/lib/polkadart_keyring.dart
+++ packages/polkadart_keyring/lib/polkadart_keyring.dart
@@ -1,6 +1,5 @@
 library polkadart_keyring;
 
-import 'dart:convert';
 import 'dart:typed_data';
 import 'package:collection/collection.dart';
 import 'package:convert/convert.dart';
@@ -10,8 +9,6 @@ import 'package:secp256k1_ecdsa/secp256k1.dart' as secp256k1;
 import 'package:ss58/ss58.dart';
 import 'package:sr25519/sr25519.dart' as sr25519;
 import 'package:pointycastle/digests/blake2b.dart' show Blake2bDigest;
-import 'package:polkadart_scale_codec/polkadart_scale_codec.dart'
-    as scale_codec;
 
 // src
 part 'src/keyring.dart';
@@ -21,7 +18,6 @@ part 'src/ecdsa.dart';
 part 'src/ed25519.dart';
 part 'src/sr25519.dart';
 part 'src/public_key.dart';
-part 'src/multisig.dart';
 
 // utils
 part 'utils/constants.dart';
