312de9fcad6ad76a9d48cbe399be7b3866a83849
Kawaljeet Singh <kawal741536@gmail.com>
2024-02-18 23:00:03 -0500

    change keypair in example

--- packages/polkadart/example/multisig_example.dart
+++ packages/polkadart/example/multisig_example.dart
@@ -6,29 +6,29 @@ import 'package:polkadart_keyring/polkadart_keyring.dart' as keyring;
 
 void main() async {
   //
-  // Signatories: TeslaS1, TeslaS2, TeslaS3
-  final TeslaS1 = await keyring.KeyPair.sr25519.fromUri('//TeslaS1');
-  TeslaS1.ss58Format = 42;
+  // Signatories: keypairS1, keypairS2, keypairS3
+  final keypairS1 = await keyring.KeyPair.sr25519.fromUri('//keypairS1');
+  keypairS1.ss58Format = 42;
 
-  final TeslaS2 = await keyring.KeyPair.sr25519.fromUri('//TeslaS2');
-  TeslaS2.ss58Format = 42;
+  final keypairS2 = await keyring.KeyPair.sr25519.fromUri('//keypairS2');
+  keypairS2.ss58Format = 42;
 
-  final TeslaS3 = await keyring.KeyPair.sr25519.fromUri('//TeslaS3');
-  TeslaS3.ss58Format = 42;
+  final keypairS3 = await keyring.KeyPair.sr25519.fromUri('//keypairS3');
+  keypairS3.ss58Format = 42;
 
-  // Recipient: TeslaR
-  final TeslaR = await keyring.KeyPair.sr25519.fromUri('//TeslaR');
-  TeslaR.ss58Format = 42;
+  // Recipient: keypairR
+  final keypairR = await keyring.KeyPair.sr25519.fromUri('//keypairR');
+  keypairR.ss58Format = 42;
 
   final provider = Provider.fromUri(Uri.parse('wss://westend-rpc.polkadot.io'));
 
   ///
   /// Create and Fund Multisig
   final multiSigResponse = await Multisig.createAndFundMultisig(
-    depositorKeyPair: TeslaS1,
-    otherSignatoriesAddressList: [TeslaS2.address, TeslaS3.address],
+    depositorKeyPair: keypairS1,
+    otherSignatoriesAddressList: [keypairS2.address, keypairS3.address],
     threshold: 2,
-    recipientAddress: TeslaR.address,
+    recipientAddress: keypairR.address,
     amount: BigInt.parse('711${'0' * 10}'), // 7.11 WND
     provider: provider,
   );
@@ -37,36 +37,36 @@ void main() async {
   final json = multiSigResponse.toJson();
 
   {
-    // Assuming TeslaS2 is the first signatory who is approving.
+    // Assuming keypairS2 is the first signatory who is approving.
     final localResponse = MultisigResponse.fromJson(json);
-    // Approve this call by TeslaS2 and forward for further approval.
+    // Approve this call by keypairS2 and forward for further approval.
     await Future.delayed(Duration(seconds: 15));
-    await localResponse.approveAsMulti(provider, TeslaS2);
+    await localResponse.approveAsMulti(provider, keypairS2);
   }
 
   /**
-   * Although TeslaS3 can call `approveAsMulti` to approve this call which will then
-   * wait for `TeslaS1` to approve via `asMulti` call.
+   * Although keypairS3 can call `approveAsMulti` to approve this call which will then
+   * wait for `keypairS1` to approve via `asMulti` call.
    * 
    * In this case:
-   * The last signatory (`TeslaS1`) will not be able to call `approveAsMulti` as it will throw FinalApprovalException.
+   * The last signatory (`keypairS1`) will not be able to call `approveAsMulti` as it will throw FinalApprovalException.
    * 
-   * So, `TeslaS1` will have to call `asMulti` to approve this call.
+   * So, `keypairS1` will have to call `asMulti` to approve this call.
    */
 
   {
-    // Assuming TeslaS3 is the second signatory who is approving.
+    // Assuming keypairS3 is the second signatory who is approving.
     final localResponse = MultisigResponse.fromJson(json);
-    // Execute this call by TeslaS3 approval.
+    // Execute this call by keypairS3 approval.
     await Future.delayed(Duration(seconds: 15));
-    await localResponse.asMulti(provider, TeslaS3);
+    await localResponse.asMulti(provider, keypairS3);
   }
 
-  // // Cancel this call by TeslaS1
+  // // Cancel this call by keypairS1
   //
   // final localResponse = MultisigResponse.fromJson(json);
   //
-  // // Cancel this call by TeslaS1
+  // // Cancel this call by keypairS1
   // await Future.delayed(Duration(seconds: 15));
-  // await multiSigResponse.cancelAsMulti(provider, TeslaS1);
+  // await multiSigResponse.cancelAsMulti(provider, keypairS1);
 }
