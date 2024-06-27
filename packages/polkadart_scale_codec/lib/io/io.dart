2b3416c3d88eeb6f5874bc0d158931bd832c8eff
Leonardo Custodio <leonardo@enjin.io>
2024-06-27 15:47:34 -0300

    Fixes build & tests (#478)
    * Some changes
    
    * More changes
    
    * Changes
    
    * Fixes
    
    * Some fixes
    
    * fix typo
    
    * Adds dart run
    
    * Revert
    
    * return to top dir
    
    * test

--- packages/polkadart_scale_codec/lib/io/io.dart
+++ packages/polkadart_scale_codec/lib/io/io.dart
@@ -1,13 +1,14 @@
 library io;
 
-import 'dart:typed_data' show Uint8List, UnmodifiableUint8ListView;
+import 'dart:typed_data' show Uint8List;
+
 import 'package:typed_data/typed_buffers.dart';
 
 import '../utils/utils.dart' show encodeHex, decodeHex;
 
 part './byte_output.dart';
+part './generator_output.dart';
 part './hex_output.dart';
 part './input.dart';
 part './output.dart';
 part './size_tracker.dart';
-part './generator_output.dart';
