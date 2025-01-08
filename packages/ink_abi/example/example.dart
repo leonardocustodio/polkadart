a647cbc64ea45eea67e13bc6050daa0a8aa5f622
Kawaljeet Singh <kawal741536@gmail.com>
2025-01-07 23:06:35 -0500

    Fix example code

--- packages/ink_abi/example/example.dart
+++ packages/ink_abi/example/example.dart
@@ -3,9 +3,8 @@ import 'dart:io';
 import 'package:ink_abi/ink_abi_base.dart';
 
 void main() {
-  final jsonFile =
-      '/Users/kawal/Desktop/git_projects/polkadart/packages/ink_abi/example/v3_metadata.json';
-  final json = jsonDecode(File(jsonFile).readAsStringSync());
-  final jsonMap = SchemaValidator.getInkProject(json);
-  final inkAbi = InkAbi(jsonMap);
+  final String dir = Directory.current.absolute.path;
+  final String jsonFilePath = '$dir/example/v3_metadata.json';
+  final json = jsonDecode(File(jsonFilePath).readAsStringSync());
+  final InkAbi inkAbi = InkAbi(json);
 }
