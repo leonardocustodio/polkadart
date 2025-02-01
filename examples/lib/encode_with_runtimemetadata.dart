1c82d62ea4c43837f51bbb393b76d97108d01ba4
Leonardo Custodio <leonardo@custodio.me>
2025-02-01 14:02:01 -0300

    metadata_example

---
+++ examples/lib/encode_with_runtimemetadata.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: non_constant_identifier_names
+import 'package:polkadart/apis/apis.dart';
+import 'package:polkadart/polkadart.dart'
+    show
+        Provider,
+        StateApi;
+import 'package:substrate_metadata/substrate_metadata.dart';
+
+Future<void> main(List<String> arguments) async {
+  final provider = Provider.fromUri(Uri.parse('wss://rpc.polkadot.io'));
+  final state = StateApi(provider);
+  final runtimeMetadata = (await state.getMetadata()).metadata;
+
+  // Let's use the runtime metadata to check how an extrinsic looks like
+  final callType = runtimeMetadata.pallets[6].calls; // Pallet 6 = Balances pallet
+  final callTypeDef = runtimeMetadata.typeById(callType!.type).type.typeDef as TypeDefVariant;
+  final variants = Map<String, Variant>.fromEntries(callTypeDef.variants.map((variant) {
+    print('Balance call #${variant.index}: ${variant.name}');
+    return MapEntry(variant.name, variant);
+  }));
+
+  final transferAll = variants['transfer_all']!;
+  print('Let\'s check the fields of the transfer_all variant');
+  transferAll.fields.forEach((field) {
+    print('Field of the extrinsic: ${field.name} - ${field.typeName}');
+  });
+}
