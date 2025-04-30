5cf3700e587f5816f6d2944541a9ef34273c8e47
Leonardo Custodio <leonardo@custodio.me>
2025-04-30 11:41:01 -0300

    New landing page for our docs

---
+++ docs/src/components/snippet/provider.dart
@@ -0,0 +1,9 @@
+import 'package:polkadart/polkadart.dart' show Provider, StateApi;
+
+void main() async {
+    final provider = Provider.fromUri(Uri.parse('wss://rpc.polkadot.io'));
+    final stateApi = StateApi(provider);
+    final runtimeVersion = await stateApi.getRuntimeVersion();
+
+    print(runtimeVersion.toJson());
+}
