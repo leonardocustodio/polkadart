f6676ab319adeeeb326ae58a51c77081184959ca
Leonardo Custodio <leonardo@custodio.me>
2025-07-28 20:37:40 -0300

    Format

--- docs/src/components/snippet/provider.dart
+++ docs/src/components/snippet/provider.dart
@@ -1,9 +1,9 @@
 import 'package:polkadart/polkadart.dart' show Provider, StateApi;
 
 void main() async {
-    final provider = Provider.fromUri(Uri.parse('wss://rpc.polkadot.io'));
-    final stateApi = StateApi(provider);
-    final runtimeVersion = await stateApi.getRuntimeVersion();
+  final provider = Provider.fromUri(Uri.parse('wss://rpc.polkadot.io'));
+  final stateApi = StateApi(provider);
+  final runtimeVersion = await stateApi.getRuntimeVersion();
 
-    print(runtimeVersion.toJson());
+  print(runtimeVersion.toJson());
 }
