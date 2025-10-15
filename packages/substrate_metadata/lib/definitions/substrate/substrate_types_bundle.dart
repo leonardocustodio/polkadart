80fde8b321639c10eae6ea1549befed32a921694
Kawaljeet Singh <kawal741536@gmail.com>
2025-10-14 22:55:47 -0400

    Drop Legacy Types and Definitions (V9-V13)

--- packages/substrate_metadata/lib/definitions/substrate/substrate_types_bundle.dart
+++
@@ -1,6 +0,0 @@
-import '../../models/legacy_types.dart';
-import 'substrate_types.dart';
-import 'substrate_types_alias.dart';
-
-final substrateTypesBundle =
-    LegacyTypesBundle(types: substrateTypes, typesAlias: substrateTypesAlias);
