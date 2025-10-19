f2a2ce2c52687e59f3c02da7d5e6eb3f1b77b018
Kawaljeet Singh <kawal741536@gmail.com>
2025-10-18 21:05:49 -0400

    Add toHex method for Uint8List and List<int>

--- packages/substrate_metadata/lib/utils/utils.dart
+++ packages/substrate_metadata/lib/utils/utils.dart
@@ -1,7 +1,8 @@
 library utils;
 
 import 'dart:convert';
-
+import 'dart:typed_data' show Uint8List;
+import 'package:convert/convert.dart' show hex;
 import 'package:cross_file/cross_file.dart';
 import 'package:polkadart_scale_codec/polkadart_scale_codec.dart';
 
