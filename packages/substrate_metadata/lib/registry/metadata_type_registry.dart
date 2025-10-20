581b74274180c70b2fdb051a23c91169fdc0789f
Kawaljeet Singh <kawal741536@gmail.com>
2025-10-19 20:55:06 -0400

    Update Metadata Registry

--- packages/substrate_metadata/lib/registry/metadata_type_registry.dart
+++ packages/substrate_metadata/lib/registry/metadata_type_registry.dart
@@ -619,3 +619,19 @@ class MetadataException implements Exception {
   @override
   String toString() => 'MetadataException: $message';
 }
+
+/// Portable registry wrapper for cleaner API
+class PortableRegistry {
+  final List<PortableType> types;
+
+  PortableRegistry(this.types);
+
+  PortableType? getType(int id) {
+    for (final type in types) {
+      if (type.id == id) {
+        return type;
+      }
+    }
+    return null;
+  }
+}
--- packages/substrate_metadata/lib/substrate_metadata.dart
+++ packages/substrate_metadata/lib/substrate_metadata.dart
@@ -1,3 +1,5 @@
 library substrate_metadata;
 
 export 'metadata/metadata.dart';
+export 'utils/utils.dart';
+export 'registry/metadata_type_registry.dart';
